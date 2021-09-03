9f0392290002324fd814dc7a7a8b3327a8588809
Christian Gama <christiangama.dev@gmail.com>
2021-09-03 12:19:28 -0300

    feat: ensure all methods from AccountValidator returns true if succeds

--- src/application/validators/account-validator.ts
+++ src/application/validators/account-validator.ts
@@ -1,23 +1,31 @@
 import { MissingParamError, InvalidParamError } from '@/application/errors'
 
 export class AccountValidator {
-  validateEmail (email: string): Error | undefined {
+  validateEmail (email: string): Error | true {
     if (!email.includes('@')) return new InvalidParamError('email')
+
+    return true
   }
 
-  validateName (name: string): Error | undefined {
+  validateName (name: string): Error | true {
     if (name.match(/[^a-zA-Z ]/g)) return new InvalidParamError('name')
+
+    return true
   }
 
-  validatePassword (password: string): Error | undefined {
+  validatePassword (password: string): Error | true {
     if (password.length < 6 || password.length > 32) return new InvalidParamError('password')
+
+    return true
   }
 
-  validatePasswordEquality (password: string, passwordConfirmation: string): Error | undefined {
+  validatePasswordEquality (password: string, passwordConfirmation: string): Error | true {
     if (password !== passwordConfirmation) return new InvalidParamError('passwordConfirmation')
+
+    return true
   }
 
-  validateRequiredFields (input: Record<string, unknown>): Error | undefined {
+  validateRequiredFields (input: Record<string, unknown>): Error | true {
     if (!input.email) return new MissingParamError('email')
 
     if (!input.name) return new MissingParamError('name')
@@ -25,5 +33,7 @@ export class AccountValidator {
     if (!input.password) return new MissingParamError('password')
 
     if (!input.passwordConfirmation) return new MissingParamError('passwordConfirmation')
+
+    return true
   }
 }
