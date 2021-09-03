85f9cb38bbe892c79d52b4f41d6ae8937f0a8be4
Christian Gama <christiangama.dev@gmail.com>
2021-09-02 22:46:19 -0300

    chore: add correct moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,15 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    '@/domain/(.*)': ['<rootDir>/src/domain/$1'],
+    '@/application/(.*)': ['<rootDir>/src/application/$1'],
+    '@/infraestructure/(.*)': ['<rootDir>/src/infraestructure/$1'],
+    '@/main/(.*)': ['<rootDir>/src/main/$1'],
+    '@/persitence/(.*)': ['<rootDir>/src/persitence/$1'],
+    '@/presentation/(.*)': ['<rootDir>/src/presentation/$1']
   }
 }
