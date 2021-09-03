766d0e5a6bf88b46dda510aefd09194e029d5bbb
Christian Gama <christiangama.dev@gmail.com>
2021-09-03 12:50:38 -0300

    refactor: remove unused function

--- src/presentation/http/http.ts
+++ src/presentation/http/http.ts
@@ -1,12 +1,5 @@
 import { HttpResponse } from './protocols'
 
-export const badRequest = (statusCode: number, error: Error): HttpResponse => {
-  return {
-    statusCode,
-    data: { error }
-  }
-}
-
 export const ok = (data: any): HttpResponse => {
   return {
     statusCode: 200,
