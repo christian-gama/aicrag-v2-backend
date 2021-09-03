f6637ee79e95d191f1e9f940f15597ed1f4a2b99
Christian Gama <christiangama.dev@gmail.com>
2021-09-03 20:31:26 -0300

    refactor: change Account protocol to AccountProtocol

--- src/domain/account/account-protocol.ts
+++ src/domain/account/account-protocol.ts
@@ -1,7 +1,7 @@
 import { UserAccount } from '@/domain/user/user-account-protocol'
 import { User } from '@/domain/user/user-protocol'
 
-export interface Account {
+export interface AccountProtocol {
   add: (account: UserAccount) => Promise<User>
   get: (id: User['personal']['id']) => Promise<User>
   update: (id: User['personal']['id']) => Promise<User>
