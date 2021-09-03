6be6511a070b61c51d2e9ccfa676ca704c0b720d
Christian Gama <christiangama.dev@gmail.com>
2021-09-03 11:51:27 -0300

    feat: return InvalidParamError if a field is invalid

--- src/application/validators/account-validator.ts
+++ src/application/validators/account-validator.ts
@@ -1,7 +1,19 @@
-import { InvalidParamError } from '../errors/invalid-param-error'
+import { InvalidParamError } from '@/application/errors/invalid-param-error'
 
 export class AccountValidator {
   validateEmail (email: string): Error | undefined {
     if (!email.includes('@')) return new InvalidParamError('email')
+  }
+
+  validateName (name: string): Error | undefined {
+    if (name.match(/[^a-zA-Z ]/g)) return new InvalidParamError('name')
+  }
+
+  validatePassword (password: string): Error | undefined {
+    if (password.length < 6 || password.length > 32) return new InvalidParamError('password')
+  }
+
+  validatePasswordEquality (password: string, passwordConfirmation: string): Error | undefined {
+    if (password !== passwordConfirmation) return new InvalidParamError('passwordConfirmation')
   }
 }
