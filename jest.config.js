fe9a210136404f7aeeb02ad3779d9fcd03a5fa75
Christian Gama <christiangama.dev@gmail.com>
2021-09-03 12:47:59 -0300

    chore: edit the module root path

--- jest.config.js
+++ jest.config.js
@@ -10,11 +10,11 @@ module.exports = {
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
-    '@/domain/(.*)': ['<rootDir>/src/domain/$1'],
-    '@/application/(.*)': ['<rootDir>/src/application/$1'],
-    '@/infraestructure/(.*)': ['<rootDir>/src/infraestructure/$1'],
-    '@/main/(.*)': ['<rootDir>/src/main/$1'],
-    '@/persitence/(.*)': ['<rootDir>/src/persitence/$1'],
-    '@/presentation/(.*)': ['<rootDir>/src/presentation/$1']
+    '@/domain/(.*)': '<rootDir>/src/domain/$1',
+    '@/application/(.*)': '<rootDir>/src/application/$1',
+    '@/infraestructure/(.*)': '<rootDir>/src/infraestructure/$1',
+    '@/main/(.*)': '<rootDir>/src/main/$1',
+    '@/persitence/(.*)': '<rootDir>/src/persitence/$1',
+    '@/presentation/(.*)': '<rootDir>/src/presentation/$1'
   }
 }
