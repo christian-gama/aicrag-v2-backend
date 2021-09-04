6c22195e8c7121c69701dc95ecc2e8f91e4e2850
Christian Gama <christiangama.dev@gmail.com>
2021-09-04 16:41:29 -0300

    refactor: remove useless if statement

--- src/application/validators/account/compare-passwords.ts
+++ src/application/validators/account/compare-passwords.ts
@@ -3,8 +3,6 @@ import { Validation } from '../protocols/validation-protocol'
 
 export class ComparePasswords implements Validation {
   validate (input: any): Error | undefined {
-    if (input.password === '' || input.passwordConfirmation === '') return new InvalidParamError('passwordConfirmation')
-
-    if (input.password !== input.passwordConfirmation) return new InvalidParamError('passwordConfirmation')
+    if (input.password !== input.passwordConfirmation) { return new InvalidParamError('passwordConfirmation') }
   }
 }
