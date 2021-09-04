9eac9ca1235ded20de0b984679d33efb033ac45e
Christian Gama <christiangama.dev@gmail.com>
2021-09-04 14:24:49 -0300

    test: ensure hash return a hashed string value

---
+++ tests/infra/adapters/bcrypt/bcrypt-adapter.spec.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import { makeSut } from './mocks/bcrypt-adapter-mock'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return Promise.resolve('hashed_value')
+  }
+}))
+
+describe('BcryptAdapter', () => {
+  it('Should call bcrypt with correct values', async () => {
+    const { sut, salt } = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+
+    await sut.hash('value')
+
+    expect(hashSpy).toHaveBeenCalledWith('value', salt)
+  })
+})
