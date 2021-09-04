aa6f789ab115e5c38699db4ee5027a26885384d9
Christian Gama <christiangama.dev@gmail.com>
2021-09-04 16:26:00 -0300

    test: ensure generate returns random digits

--- tests/application/helpers/code-generator/code-generator.spec.ts
+++ tests/application/helpers/code-generator/code-generator.spec.ts
@@ -1,16 +1,38 @@
 import { makeSut } from './mocks/code-generator-mock'
 
 describe('CodeGenerator', () => {
-  it('Should return 5 random digits', () => {
+  it('Should return 5 digits', () => {
     const sut = makeSut()
 
     let error = 0
-    for (let i = 0; i < 5000; i++) {
+    for (let i = 0; i < 100; i++) {
       const value = sut.generate()
 
       if (value.length !== 5) error++
     }
 
     expect(error).toBe(0)
+  })
+
+  it('Should return random digits', () => {
+    const sut = makeSut()
+
+    const values: Partial<string[]> = []
+    for (let i = 0; i < 10; i++) {
+      const value = sut.generate()
+      values.push(value)
+    }
+
+    const filteredValues = [...new Set(values)]
+
+    expect(filteredValues.length).not.toBe(1)
+  })
+
+  it('Should return a string', () => {
+    const sut = makeSut()
+
+    const value = sut.generate()
+
+    expect(typeof value).toBe('string')
   })
 })
