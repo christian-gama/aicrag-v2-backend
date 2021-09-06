5a6a9e95b41c451a2330974c3f4018a5f9f6ef68
Christian Gama <christiangama.dev@gmail.com>
2021-09-06 20:48:54 -0300

    test: ensure call validate with correct values

--- tests/presentation/controllers/signup/mocks/signup-controller-mock.ts
+++ tests/presentation/controllers/signup/mocks/signup-controller-mock.ts
@@ -1,9 +1,10 @@
 import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
+import { AccountValidatorProtocol } from '@/application/protocols/validators/account/account-validator-protocol'
 import { User, UserAccount } from '@/domain/user'
 import { SignUpController } from '@/presentation/controllers/signup/signup-controllers'
 import { makeFakeUser } from '@/tests/domain/mocks/user-mock'
 
-const makeAccountDbRepository = (): AccountDbRepositoryProtocol => {
+const makeAccountDbRepositoryStub = (): AccountDbRepositoryProtocol => {
   class AccountDbRepositoryStub implements AccountDbRepositoryProtocol {
     async saveAccount (account: UserAccount): Promise<User> {
       return Promise.resolve(makeFakeUser())
@@ -13,14 +14,26 @@ const makeAccountDbRepository = (): AccountDbRepositoryProtocol => {
   return new AccountDbRepositoryStub()
 }
 
+const makeAccountValidatorStub = (): AccountValidatorProtocol => {
+  class AccountValidatorStub implements AccountValidatorProtocol {
+    validate (input: any): Error | undefined {
+      return undefined
+    }
+  }
+
+  return new AccountValidatorStub()
+}
+
 export interface SutTypes {
   sut: SignUpController
   accountDbRepositoryStub: AccountDbRepositoryProtocol
+  accountValidatorStub: AccountValidatorProtocol
 }
 
 export const makeSut = (): SutTypes => {
-  const accountDbRepositoryStub = makeAccountDbRepository()
-  const sut = new SignUpController(accountDbRepositoryStub)
+  const accountDbRepositoryStub = makeAccountDbRepositoryStub()
+  const accountValidatorStub = makeAccountValidatorStub()
+  const sut = new SignUpController(accountDbRepositoryStub, accountValidatorStub)
 
-  return { sut, accountDbRepositoryStub }
+  return { sut, accountDbRepositoryStub, accountValidatorStub }
 }
--- tests/presentation/controllers/signup/signup-controllers.spec.ts
+++ tests/presentation/controllers/signup/signup-controllers.spec.ts
@@ -11,4 +11,14 @@ describe('SignUpController', () => {
 
     expect(saveAccountSpy).toHaveBeenCalledWith(request.body)
   })
+
+  it('Should call validate with correct values', async () => {
+    const { sut, accountValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(accountValidatorStub, 'validate')
+    const request = { body: fakeValidAccount }
+
+    await sut.handle(request)
+
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
 })
