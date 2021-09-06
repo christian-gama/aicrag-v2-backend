a866124ea4d69f162c57a53d8439597f3f444068
Christian Gama <christiangama.dev@gmail.com>
2021-09-06 20:48:39 -0300

    feat: ensure call validate with correct values

--- src/presentation/controllers/signup/signup-controllers.ts
+++ src/presentation/controllers/signup/signup-controllers.ts
@@ -1,12 +1,18 @@
 import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
+import { AccountValidatorProtocol } from '@/application/protocols/validators/account/account-validator-protocol'
 import { HttpRequest, HttpResponse } from '@/presentation/http/protocols'
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class SignUpController implements ControllerProtocol {
-  constructor (private readonly accountDbRepository: AccountDbRepositoryProtocol) {}
+  constructor (
+    private readonly accountDbRepository: AccountDbRepositoryProtocol,
+    private readonly accountValidator: AccountValidatorProtocol
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const account = httpRequest.body
+
+    this.accountValidator.validate(account)
 
     await this.accountDbRepository.saveAccount(account)
 
