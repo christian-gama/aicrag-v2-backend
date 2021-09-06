7d942342c64ac50cde94ffba06df34dff151d4f2
Christian Gama <christiangama.dev@gmail.com>
2021-09-05 21:55:24 -0300

    feat: implement isEmail method from validator

--- src/infra/adapters/validators/email-validator/email-validator-adapter.ts
+++ src/infra/adapters/validators/email-validator/email-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { EmailValidator } from '@/application/protocols/validators/email-validator/email-validator-protocol'
 
+import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
   isEmail (value: string): boolean {
-    return true
+    return validator.isEmail(value)
   }
 }
