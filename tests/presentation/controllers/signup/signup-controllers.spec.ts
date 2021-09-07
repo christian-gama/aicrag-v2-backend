58441f176a63c34a06d35f16956c1943128f33c0
Christian Gama <christiangama.dev@gmail.com>
2021-09-07 16:01:59 -0300

    refactor: remove duplicated code

--- tests/presentation/controllers/signup/signup-controllers.spec.ts
+++ tests/presentation/controllers/signup/signup-controllers.spec.ts
@@ -20,34 +20,6 @@ describe('SignUpController', () => {
     expect(validateSpy).toHaveBeenCalledWith(request.body)
   })
 
-  it('Should return a bad request if validation fails with an error message', async () => {
-    const { sut, accountValidatorStub, request } = makeSut()
-    const error = new InvalidParamError('any_field')
-    jest.spyOn(accountValidatorStub, 'validate').mockReturnValueOnce(error)
-
-    const response = await sut.handle(request)
-
-    expect(response).toEqual({ statusCode: 400, data: { message: error.message } })
-  })
-
-  it('Should return a user if validation succeds', async () => {
-    const { sut, request } = makeSut()
-
-    const response = await sut.handle(request)
-
-    expect(response.data.user.personal.name).toBe(request.body.name)
-    expect(response.data.user.personal.email).toBe(request.body.email)
-  })
-
-  it('Should call ok with the correct value', async () => {
-    const { sut, fakeUser, httpHelper, request } = makeSut()
-    const okSpy = jest.spyOn(httpHelper, 'ok')
-
-    await sut.handle(request)
-
-    expect(okSpy).toHaveBeenCalledWith({ user: fakeUser })
-  })
-
   it('Should call badRequest with the correct value', async () => {
     const { sut, httpHelper, accountValidatorStub, request } = makeSut()
 
@@ -59,5 +31,32 @@ describe('SignUpController', () => {
     await sut.handle(request)
 
     expect(badRequestSpy).toHaveBeenCalledWith(error)
+  })
+
+  it('Should return badRequest if validation fails with an error message', async () => {
+    const { sut, accountValidatorStub, request, httpHelper } = makeSut()
+    const error = new InvalidParamError('any_field')
+    jest.spyOn(accountValidatorStub, 'validate').mockReturnValueOnce(error)
+
+    const response = await sut.handle(request)
+
+    expect(response).toEqual(httpHelper.badRequest(error))
+  })
+
+  it('Should call ok with the correct value', async () => {
+    const { sut, fakeUser, httpHelper, request } = makeSut()
+    const okSpy = jest.spyOn(httpHelper, 'ok')
+
+    await sut.handle(request)
+
+    expect(okSpy).toHaveBeenCalledWith({ user: fakeUser })
+  })
+
+  it('Should return ok if validation succeds', async () => {
+    const { sut, request, httpHelper, fakeUser } = makeSut()
+
+    const response = await sut.handle(request)
+
+    expect(response).toEqual(httpHelper.ok({ user: fakeUser }))
   })
 })
