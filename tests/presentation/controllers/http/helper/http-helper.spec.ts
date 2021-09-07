d36b20bcb9d7951c229b42f24162c18fecb85d53
Christian Gama <christiangama.dev@gmail.com>
2021-09-07 14:44:01 -0300

    test: ensure notFound return a statusCode and an error message

--- tests/presentation/controllers/http/helper/http-helper.spec.ts
+++ tests/presentation/controllers/http/helper/http-helper.spec.ts
@@ -3,12 +3,21 @@ import { makeSut } from './mocks/http-helper-mock'
 import faker from 'faker'
 
 describe('HttpHelper', () => {
-  it('Should return 400 and an error when calls badRequest', () => {
+  it('Should return 400 and an error message when calls badRequest', () => {
     const sut = makeSut()
     const errorMessage = faker.lorem.words(3)
 
     const response = sut.badRequest(new Error(errorMessage))
 
     expect(response).toEqual({ statusCode: 400, data: { message: errorMessage } })
+  })
+
+  it('Should return 404 and an error message when calls notFound', () => {
+    const sut = makeSut()
+    const errorMessage = faker.lorem.words(3)
+
+    const response = sut.notFound(new Error(errorMessage))
+
+    expect(response).toEqual({ statusCode: 404, data: { message: errorMessage } })
   })
 })
