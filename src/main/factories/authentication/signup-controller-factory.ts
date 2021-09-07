2e38c3f732d2e876c9f65081fc01a49621861d9c
Christian Gama <christiangama.dev@gmail.com>
2021-09-07 19:13:49 -0300

    refactor: create a factory method for SignUpController

---
+++ src/main/factories/authentication/signup-controller-factory.ts
@@ -0,0 +1,13 @@
+import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
+import { SignUpController } from '@/presentation/controllers/signup/signup-controllers'
+import { HttpHelper } from '@/presentation/http/helper/http-helper'
+import { makeAccountDbRepository } from '../account/account-db-repository/account-db-repository-factory'
+import { makeAccountValidatorComposite } from '../account/account-validator/account-validator-composite-factory'
+
+export const makeSignUpController = (): ControllerProtocol => {
+  const accountDbRepository = makeAccountDbRepository()
+  const accountValidator = makeAccountValidatorComposite()
+  const httpHelper = new HttpHelper()
+
+  return new SignUpController(accountDbRepository, accountValidator, httpHelper)
+}
