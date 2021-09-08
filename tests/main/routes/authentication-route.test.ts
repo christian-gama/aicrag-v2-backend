061bfef113fe769ba6c19920b4c49135faa23d52
Christian Gama <christiangama.dev@gmail.com>
2021-09-07 22:04:27 -0300

    test: ensure /auth/signup return 200 if all validation succeds

---
+++ tests/main/routes/authentication-route.test.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import app from '@/main/config/app'
+import { fakeValidAccount } from '@/tests/domain/mocks/account-mock'
+
+let accountCollection: Collection
+describe('Authentication routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('SignUp', () => {
+    it('Should return 200 if all validations succeds', async () => {
+      await request(app).post('/api/auth/signup').send(fakeValidAccount).expect(200)
+    })
+  })
+})
