dcb8eabb0ca638cd0210db2958d47e26a35c8e95
Christian Gama <christiangama.dev@gmail.com>
2021-09-07 22:06:41 -0300

    test: ensure /auth/signup return 409 if email already exists

--- tests/main/routes/authentication-route.test.ts
+++ tests/main/routes/authentication-route.test.ts
@@ -1,8 +1,10 @@
 import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
+import { fakeValidAccount } from '@/tests/domain/mocks/account-mock'
+import { makeFakeUser } from '@/tests/domain/mocks/user-mock'
+
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import app from '@/main/config/app'
-import { fakeValidAccount } from '@/tests/domain/mocks/account-mock'
 
 let accountCollection: Collection
 describe('Authentication routes', () => {
@@ -26,6 +28,19 @@ describe('Authentication routes', () => {
 
     it('Should return 400 if validation fails', async () => {
       await request(app).post('/api/auth/signup').send({}).expect(400)
+    })
+
+    it('Should return 409 if email already exists', async () => {
+      const fakeUser = makeFakeUser()
+      const fakeAccount = {
+        name: fakeUser.personal.name,
+        email: fakeUser.personal.email,
+        password: fakeUser.personal.password,
+        passwordConfirmation: fakeUser.personal.password
+      }
+      await accountCollection.insertOne(fakeUser)
+
+      await request(app).post('/api/auth/signup').send(fakeAccount).expect(409)
     })
   })
 })
