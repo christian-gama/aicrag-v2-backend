13c44326c5475065948318cc5f1507ce59fe28f8
Christian Gama <christiangama.dev@gmail.com>
2021-09-09 00:54:41 -0300

    feat: ensure validate return a UserCredentialError if credentials are invalid

---
+++ src/application/usecases/validators/credentials/validate-credentials.ts
@@ -0,0 +1,25 @@
+import { ComparerProtocol } from '@/application/protocols/cryptography'
+import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
+import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
+import { User } from '@/domain/user'
+import { UserCredentialError } from '../../errors'
+
+export class ValidateCredentials implements ValidatorProtocol {
+  constructor (
+    private readonly accountDbRepository: AccountDbRepositoryProtocol,
+    private readonly hasher: ComparerProtocol
+  ) {}
+
+  async validate (input: any): Promise<Error | undefined> {
+    const { email } = input
+
+    const user = await this.userExists(email)
+    if (!user) return new UserCredentialError()
+  }
+
+  private async userExists (email: string): Promise<User | undefined> {
+    const user = await this.accountDbRepository.findAccountByEmail(email)
+
+    return user
+  }
+}
