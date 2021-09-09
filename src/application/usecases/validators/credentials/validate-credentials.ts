0bfaea58d62d8d93f08ae00060e45b93a48bfd26
Christian Gama <christiangama.dev@gmail.com>
2021-09-09 12:11:44 -0300

    feat: ensure validate returns a UserCredentialError if passwords does not match

--- src/application/usecases/validators/credentials/validate-credentials.ts
+++ src/application/usecases/validators/credentials/validate-credentials.ts
@@ -11,15 +11,22 @@ export class ValidateCredentials implements ValidatorProtocol {
   ) {}
 
   async validate (input: any): Promise<Error | undefined> {
-    const { email } = input
+    const { email, password } = input
 
     const user = await this.userExists(email)
     if (!user) return new UserCredentialError()
+
+    const passwordMatches = await this.passwordMatch(password, user)
+    if (!passwordMatches) return new UserCredentialError()
   }
 
   private async userExists (email: string): Promise<User | undefined> {
     const user = await this.accountDbRepository.findAccountByEmail(email)
 
     return user
+  }
+
+  private async passwordMatch (password: string, user: User): Promise<boolean> {
+    return await this.hasher.compare(password, user.personal.password)
   }
 }
