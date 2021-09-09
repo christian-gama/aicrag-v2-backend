ea1be5032cbf4391fda34e49fd4fb68940da1a00
Christian Gama <christiangama.dev@gmail.com>
2021-09-08 23:27:50 -0300

    refactor: make ValidatorProtocol more generic

--- src/application/protocols/validators/validator-protocol.ts
+++ src/application/protocols/validators/validator-protocol.ts
@@ -3,5 +3,5 @@ export interface ValidatorProtocol {
    * @param input Is the object to be validated.
    * @returns Return an Error if validation fails and return undefined if succeds.
    */
-  validate: (input: Record<any, unknown>) => Error | undefined
+  validate: (input: Record<any, any>) => Error | undefined
 }
