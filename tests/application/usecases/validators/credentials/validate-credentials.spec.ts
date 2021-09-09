030257fad859927f9560dfc928f1953f68932b8b
Christian Gama <christiangama.dev@gmail.com>
2021-09-09 00:56:17 -0300

    test: ensure calls findAccountByEmail with correct value

--- tests/application/usecases/validators/credentials/validate-credentials.spec.ts
+++ tests/application/usecases/validators/credentials/validate-credentials.spec.ts
@@ -30,4 +30,14 @@ describe('ValidateCredentials', () => {
 
     expect(error).toEqual(new UserCredentialError())
   })
+
+  it('Should call findAccountByEmail with correct value', async () => {
+    const { sut, accountDbRepositoryStub } = makeSut()
+    const findAccountByEmailSpy = jest.spyOn(accountDbRepositoryStub, 'findAccountByEmail')
+    const credentials = { email: 'invalid_email@email.com', password: 'any_password' }
+
+    await sut.validate(credentials)
+
+    expect(findAccountByEmailSpy).toHaveBeenCalledWith(credentials.email)
+  })
 })
