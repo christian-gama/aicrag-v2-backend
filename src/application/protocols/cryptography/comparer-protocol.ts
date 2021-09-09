41d68940b6ba9f2914168da19ee0b29febf39280
Christian Gama <christiangama.dev@gmail.com>
2021-09-09 01:09:59 -0300

    docs: improve documentation text

--- src/application/protocols/cryptography/comparer-protocol.ts
+++ src/application/protocols/cryptography/comparer-protocol.ts
@@ -3,7 +3,7 @@ export interface ComparerProtocol {
   /**
   * @async Asynchronous method.
   * @param value The first value that will be compared to the second value.
-  * @param valueToCompare The second value that will be compared with the first value.
+  * @param valueToCompare The second value that will be compared to the first value.
   * @returns Return true if both values are valid and false if not.
   */
   compare: (value: string, valueToCompare: string) => Promise<boolean>
