016950582fb0f0c64465e7f8221ca3953748e30b
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 18:56:30 -0300

    feat: ensure calls encryptId with correct id

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,5 +1,7 @@
+import { EncrypterProtocol } from '@/application/protocols/cryptography/encrypter-protocol'
 import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
 import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
+import { User } from '@/domain/user'
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/http/protocols'
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
@@ -7,7 +9,8 @@ export class LoginController implements ControllerProtocol {
   constructor (
     private readonly accountDbRepository: AccountDbRepositoryProtocol,
     private readonly credentialsValidator: ValidatorProtocol,
-    private readonly httpHelper: HttpHelperProtocol
+    private readonly httpHelper: HttpHelperProtocol,
+    private readonly jwtAdapter: EncrypterProtocol
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -17,9 +20,9 @@ export class LoginController implements ControllerProtocol {
 
     if (error) return this.httpHelper.notFound(error)
 
-    const user = await this.accountDbRepository.findAccountByEmail(credentials.email)
+    const user = (await this.accountDbRepository.findAccountByEmail(credentials.email)) as User
 
-    const accessToken = 'any_token'
+    const accessToken = this.jwtAdapter.encryptId(user.personal.id)
 
     return this.httpHelper.ok({ user, accessToken })
   }
