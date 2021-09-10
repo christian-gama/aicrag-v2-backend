2a2e602629c36b9c20380f369ecb6beb13fef321
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 18:33:08 -0300

    test: add email validator and password validator to credential composite

--- tests/main/factories/validators/credentials/credentials-validator-composite-factory.spec.ts
+++ tests/main/factories/validators/credentials/credentials-validator-composite-factory.spec.ts
@@ -2,6 +2,7 @@ import { ValidatorProtocol } from '@/application/protocols/validators/validator-
 import { ValidationComposite } from '@/application/usecases/validators/validation-composite'
 import { makeCredentialsValidatorComposite } from '@/main/factories/validators/credentials-validator/credentials-validator-composite-factory'
 import { makeValidateCredentials } from '@/main/factories/validators/credentials-validator/validate-credential-factory'
+import { makeRequiredFields, makeValidateEmail, makeValidatePassword } from '@/main/factories/validators/account-validator'
 
 jest.mock('../../../../../src/application/usecases/validators/validation-composite.ts')
 
@@ -12,6 +13,13 @@ describe('CredentialsValidator Factory', () => {
     const validations: ValidatorProtocol[] = []
     const validateCredentials = makeValidateCredentials()
 
+    const fields = ['email', 'password']
+    for (const field of fields) {
+      validations.push(makeRequiredFields(field))
+    }
+
+    validations.push(makeValidateEmail())
+    validations.push(makeValidatePassword())
     validations.push(validateCredentials)
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
