756705d60eeefa1ae58b654c488dd0247a428bfc
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 19:02:25 -0300

    refactor: move LoginController to a factory method

---
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -0,0 +1,15 @@
+import { LoginController } from '@/presentation/controllers/login/login-controller'
+import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
+import { makeJwtAdapter } from '../../cryptography/jwt-adapter-factory'
+import { makeHttpHelper } from '../../helpers/http-helper-factory'
+import { makeAccountDbRepository } from '../../repositories/account-db-repository/account-db-repository-factory'
+import { makeCredentialsValidatorComposite } from '../../validators/credentials-validator/credentials-validator-composite-factory'
+
+export const makeLoginController = (): ControllerProtocol => {
+  const accountDbRepository = makeAccountDbRepository()
+  const credentialsValidator = makeCredentialsValidatorComposite()
+  const jwtAdapter = makeJwtAdapter()
+  const httpHelper = makeHttpHelper()
+
+  return new LoginController(accountDbRepository, credentialsValidator, httpHelper, jwtAdapter)
+}
