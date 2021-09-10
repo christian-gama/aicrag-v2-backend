f11364a17e51e4b7207de7e61b72cedb259198e6
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 20:58:41 -0300

    test: ensure return a default message if user has account inactive

---
+++ tests/application/usecases/errors/inactive-account-error.spec.ts
@@ -0,0 +1,15 @@
+import { InactiveAccountError } from '@/application/usecases/errors'
+
+describe('InactiveAccountError', () => {
+  it('Should be an instance of Error', () => {
+    const sut = new InactiveAccountError()
+
+    expect(sut).toBeInstanceOf(Error)
+  })
+
+  it('Should return a message with the field name', () => {
+    const sut = new InactiveAccountError()
+
+    expect(sut.message).toBe('Inactive account: activate to get access')
+  })
+})
