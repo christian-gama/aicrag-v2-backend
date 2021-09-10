8ebd45aab0a025001543636e79141a6884ac4305
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 18:32:49 -0300

    feat: add email validator and password validator to credential composite

--- src/main/factories/validators/credentials-validator/credentials-validator-composite-factory.ts
+++ src/main/factories/validators/credentials-validator/credentials-validator-composite-factory.ts
@@ -1,11 +1,19 @@
 import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
 import { ValidationComposite } from '@/application/usecases/validators/validation-composite'
+import { makeRequiredFields, makeValidateEmail, makeValidatePassword } from '../account-validator'
 import { makeValidateCredentials } from './validate-credential-factory'
 
 export const makeCredentialsValidatorComposite = (): ValidatorProtocol => {
   const validations: ValidatorProtocol[] = []
   const validateCredentials = makeValidateCredentials()
 
+  const fields = ['email', 'password']
+  for (const field of fields) {
+    validations.push(makeRequiredFields(field))
+  }
+
+  validations.push(makeValidateEmail())
+  validations.push(makeValidatePassword())
   validations.push(validateCredentials)
 
   const credentialsValidationComposite = new ValidationComposite(validations)
