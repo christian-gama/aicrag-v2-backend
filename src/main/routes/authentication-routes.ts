d8ec7757eb592fa3b9f0db1649e137f952868af8
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 20:44:10 -0300

    feat: ensure return 200 on login success

--- src/main/routes/authentication-routes.ts
+++ src/main/routes/authentication-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { adaptRoutes } from '../adapters/express/adapt-routes'
+import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
 import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
 const router = Router()
 
 router.post('/signup', adaptRoutes(makeSignUpController()))
+router.post('/login', adaptRoutes(makeLoginController()))
 
 export default router
