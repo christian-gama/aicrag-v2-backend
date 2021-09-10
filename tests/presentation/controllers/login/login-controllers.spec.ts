b8af303a46f9c5bb2ea84b0a6477be9c5210a35e
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 18:50:21 -0300

    test: ensure calls notFound with the correct error

--- tests/presentation/controllers/login/login-controllers.spec.ts
+++ tests/presentation/controllers/login/login-controllers.spec.ts
@@ -1,6 +1,7 @@
+import { UserCredentialError } from '@/application/usecases/errors'
 import { makeSut } from './mocks/login-controller-mock'
 
-describe('SignUpController', () => {
+describe('LoginController', () => {
   it('Should call validate with correct values', async () => {
     const { sut, credentialsValidatorStub, request } = makeSut()
     const validateSpy = jest.spyOn(credentialsValidatorStub, 'validate')
@@ -8,5 +9,18 @@ describe('SignUpController', () => {
     await sut.handle(request)
 
     expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+
+  it('Should call notFound with the correct value', async () => {
+    const { sut, httpHelper, credentialsValidatorStub, request } = makeSut()
+
+    const error = new UserCredentialError()
+    jest.spyOn(credentialsValidatorStub, 'validate').mockReturnValueOnce(error)
+
+    const notFoundSpy = jest.spyOn(httpHelper, 'notFound')
+
+    await sut.handle(request)
+
+    expect(notFoundSpy).toHaveBeenCalledWith(error)
   })
 })
