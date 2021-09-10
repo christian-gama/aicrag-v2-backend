700bf9ce2dbfe6ee15d2dc15d36933a96387f217
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 18:34:54 -0300

    feat: add res.cookie if a cookie is passed

--- src/main/adapters/express/adapt-routes.ts
+++ src/main/adapters/express/adapt-routes.ts
@@ -1,3 +1,4 @@
+import { env } from '@/main/config/env'
 import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
 import { HttpRequest, HttpResponse } from '@/presentation/http/protocols'
 import { Request, Response } from 'express'
@@ -7,6 +8,14 @@ export const adaptRoutes = (controller: ControllerProtocol) => {
     const httpRequest: HttpRequest = req
 
     const httpResponse: HttpResponse = await controller.handle(httpRequest)
+
+    if (httpResponse.accessToken) {
+      res.cookie('jwt', httpResponse.accessToken, {
+        maxAge: +env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: env.NODE_ENV === 'production'
+      })
+    }
 
     res.status(httpResponse.statusCode)
     res.json(httpResponse.data)
