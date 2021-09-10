cfef8edd1d0886b6cedb01534f8b904ad2b4b267
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 20:44:58 -0300

    test: ensure return 200 on login success

--- tests/main/routes/authentication-route.test.ts
+++ tests/main/routes/authentication-route.test.ts
@@ -5,6 +5,7 @@ import { makeFakeUser } from '@/tests/domain/mocks/user-mock'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import app from '@/main/config/app'
+import { hash } from 'bcrypt'
 
 let accountCollection: Collection
 describe('Authentication routes', () => {
@@ -41,6 +42,22 @@ describe('Authentication routes', () => {
       await accountCollection.insertOne(fakeUser)
 
       await request(app).post('/api/auth/signup').send(fakeAccount).expect(409)
+    })
+  })
+
+  describe('Login', () => {
+    it('Should return 200 if all validations succeds', async () => {
+      const fakeUser = makeFakeUser()
+      const hashedPassword = await hash(fakeUser.personal.password, 12)
+      const userPassword = fakeUser.personal.password
+      fakeUser.personal.password = hashedPassword
+
+      await accountCollection.insertOne(fakeUser)
+
+      await request(app)
+        .post('/api/auth/login')
+        .send({ email: fakeUser.personal.email, password: userPassword })
+        .expect(200)
     })
   })
 })
