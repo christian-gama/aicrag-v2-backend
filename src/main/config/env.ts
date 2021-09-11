84ee9a5ef0a35ba2bfa0de1ca9c6640a88d30540
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 22:28:31 -0300

    feat: add a secret to cookies

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,6 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV ?? 'production',
   JWT_SECRET: process.env.JWT_SECRET ?? 'Ql818uILlkG8a8sdklajlm112Ac4yuuX',
   JWT_EXPIRES: process.env.JWT_EXPIRES ?? '30d',
-  COOKIE_EXPIRES: process.env.COOKIE_EXPIRES ?? '30'
+  COOKIE_EXPIRES: process.env.COOKIE_EXPIRES ?? '30',
+  COOKIE_SECRET: process.env.COOKIE_SECRET ?? '1X811PvLlkG8a9s1Elajlh142Z34YWum'
 }
