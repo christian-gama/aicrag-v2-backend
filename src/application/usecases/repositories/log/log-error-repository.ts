69c4ceafecd09ea19d53cad041b5f69c94ccbdaa
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 16:58:14 -0300

    feat: ensure createError return a log error

---
+++ src/application/usecases/repositories/log/log-error-repository.ts
@@ -0,0 +1,13 @@
+import { LogErrorProtocol } from '@/domain/log/log-error-protocol'
+import { LogErrorRepositoryProtocol } from '@/application/protocols/repositories/log/log-error-repository-protocol'
+
+export class LogErrorRepository implements LogErrorRepositoryProtocol {
+  createError (error: Error): LogErrorProtocol {
+    return {
+      name: error.name,
+      date: new Date(Date.now()).toLocaleString(),
+      message: error.message,
+      stack: error.stack
+    }
+  }
+}
