cd687b8f2881f9dc8b1edba8f153638cb52a19b0
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 13:29:48 -0300

    refactor: move app.post out of test closure

--- tests/main/middlewares/body-parser.test.ts
+++ tests/main/middlewares/body-parser.test.ts
@@ -2,14 +2,15 @@ import request from 'supertest'
 import app from '@/main/config/app'
 
 describe('BodyParser', () => {
-  it('Should parse body', async () => {
+  beforeEach(() => {
     app.post('/use_body_parser', (req, res) => {
       res.send(req.body)
     })
+  })
 
-    await request(app)
-      .post('/use_body_parser')
-      .send({ name: 'any_name' })
-      .expect({ name: 'any_name' })
+  const agent = request.agent(app)
+
+  it('Should parse body', async () => {
+    await agent.post('/use_body_parser').send({ name: 'any_name' }).expect({ name: 'any_name' })
   })
 })
