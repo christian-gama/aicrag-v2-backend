f8210cb122a259ecaf4ee175a1b7f1ddfed81154
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 17:32:06 -0300

    test: ensure saveLog return a log error

--- tests/infra/database/mongodb/log/log-error-db-repository.spec.ts
+++ tests/infra/database/mongodb/log/log-error-db-repository.spec.ts
@@ -1,6 +1,25 @@
+import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
+import { env } from '@/main/config/env'
 import { makeSut } from './mocks/log-error-db-repository-mock'
 
+import { Collection } from 'mongodb'
+
 describe('LogErrorDbRepository', () => {
+  let logCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(env.DB.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    logCollection = await MongoHelper.getCollection('logs')
+    await logCollection.deleteMany({})
+  })
+
   it('Should call createLog with correct error', async () => {
     const { sut, error, logErrorRepositoryStub } = makeSut()
     const createLogSpy = jest.spyOn(logErrorRepositoryStub, 'createLog')
@@ -8,5 +27,16 @@ describe('LogErrorDbRepository', () => {
     await sut.saveLog(error)
 
     expect(createLogSpy).toHaveBeenCalledWith(error)
+  })
+
+  it('Should save a log error on database', async () => {
+    const { sut, error, fakeLogError } = makeSut()
+
+    const value = await sut.saveLog(error)
+
+    expect(value.name).toEqual(fakeLogError.name)
+    expect(value.date).toEqual(fakeLogError.date)
+    expect(value.message).toEqual(fakeLogError.message)
+    expect(value.stack).toEqual(fakeLogError.stack)
   })
 })
