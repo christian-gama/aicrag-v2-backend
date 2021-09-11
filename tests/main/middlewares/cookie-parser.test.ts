b17063c5d535bc5b123e7a4dbe532afb52543e70
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 13:33:40 -0300

    test: ensure cookie is saved

---
+++ tests/main/middlewares/cookie-parser.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('CookieParser', () => {
+  beforeEach(() => {
+    app.get('/save_cookie', (req, res) => {
+      res.cookie('any_cookie', 'any_value')
+      res.send()
+    })
+
+    app.get('/send_cookie', (req, res) => {
+      if (req.cookies.any_cookie) res.send(req.cookies.any_cookie)
+      else res.send('No cookies')
+    })
+  })
+
+  const agent = request.agent(app)
+
+  it('Should save cookies', async () => {
+    await agent.get('/save_cookie').expect('set-cookie', 'any_cookie=any_value; Path=/')
+  })
+})
