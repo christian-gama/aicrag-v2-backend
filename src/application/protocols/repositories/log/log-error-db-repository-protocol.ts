748268ad3373edb695a8bc2e9c3d5d49070772ea
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 17:30:59 -0300

    feat: ensure saveLog return a log error

--- src/application/protocols/repositories/log/log-error-db-repository-protocol.ts
+++ src/application/protocols/repositories/log/log-error-db-repository-protocol.ts
@@ -1,5 +1,5 @@
 import { LogErrorProtocol } from '@/domain/log/log-error-protocol'
 
 export interface LogErrorDbRepositoryProtocol {
-  saveLog: (log: LogErrorProtocol) => Promise<void>
+  saveLog: (log: LogErrorProtocol) => Promise<LogErrorProtocol>
 }
