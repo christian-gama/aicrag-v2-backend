8270925d939c443296aa3d89dbc93e27467b87cc
Christian Gama <christiangama.dev@gmail.com>
2021-09-10 22:29:41 -0300

    feat: include cookieParser in middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import { bodyParser, urlEncoded, cors } from '../middlewares'
+import cookieParser from '../middlewares/cookie-parser'
 
 export default (app: Express): void => {
+  app.use(cookieParser)
   app.use(bodyParser)
   app.use(urlEncoded)
   app.use(cors)
