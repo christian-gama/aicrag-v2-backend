f9bf6bf1dba2adbb428c1b50b379c0eceeb58755
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 22:11:08 -0300

    feat: ensure decorates controllers with Try-Catch statment

---
+++ src/main/decorators/try-catch-controller-decorator.ts
@@ -0,0 +1,19 @@
+import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
+import { HttpRequest, HttpResponse } from '@/presentation/http/protocols'
+import { makeHttpHelper } from '../factories/helpers/http-helper-factory'
+
+export class TryCatchControllerDecorator implements ControllerProtocol {
+  constructor (private readonly controller: ControllerProtocol) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const httpResponse = await this.controller.handle(httpRequest)
+
+      return httpResponse
+    } catch (error) {
+      const httpHelper = makeHttpHelper()
+
+      return httpHelper.serverError(error)
+    }
+  }
+}
