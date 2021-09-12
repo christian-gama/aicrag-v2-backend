f2891c3c2b93cbdc7918ef3d830f9208643f9ca9
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 22:11:25 -0300

    test: ensure decorates controllers with Try-Catch statment

---
+++ tests/main/decorators/try-catch-decorator/mocks/try-catch-controller-decorator-mock.ts
@@ -0,0 +1,30 @@
+import { makeHttpHelper } from '@/main/factories/helpers/http-helper-factory'
+import { ControllerProtocol } from '@/presentation/controllers/login'
+import { HttpHelper } from '@/presentation/http/helper/http-helper'
+import { HttpRequest, HttpResponse } from '@/presentation/http/protocols'
+import { TryCatchControllerDecorator } from '@/main/decorators/try-catch-controller-decorator'
+
+const httpHelper = makeHttpHelper()
+
+const makeControllerStub = (): ControllerProtocol => {
+  class ControllerStub implements ControllerProtocol {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return httpHelper.ok({})
+    }
+  }
+
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: TryCatchControllerDecorator
+  controllerStub: ControllerProtocol
+  httpHelper: HttpHelper
+}
+
+export const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new TryCatchControllerDecorator(controllerStub)
+
+  return { sut, controllerStub, httpHelper }
+}
---
+++ tests/main/decorators/try-catch-decorator/try-catch-controller-decorator.spec.ts
@@ -0,0 +1,28 @@
+import { makeSut } from './mocks/try-catch-controller-decorator-mock'
+
+describe('LogControllerDecorator', () => {
+  it('Should call handle witch correct value', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const request = {}
+
+    await sut.handle(request)
+
+    expect(handleSpy).toHaveBeenCalledWith(request)
+  })
+
+  it('Should return a serverError as http response if statusCode is 500', async () => {
+    const { sut, httpHelper, controllerStub } = makeSut()
+    const error = new Error('any_message')
+    jest.spyOn(controllerStub, 'handle').mockImplementationOnce(async () => Promise.reject(error))
+    const errorData = {
+      name: error.name,
+      message: error.message,
+      stack: error.stack
+    }
+
+    const promise = await sut.handle({})
+
+    expect(promise).toEqual(httpHelper.serverError(errorData))
+  })
+})
