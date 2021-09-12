a6b2049b133b4f804d4b3653f58727e32d38167a
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 23:57:55 -0300

    refactor: move agent creation out from describe

--- tests/main/middlewares/body-parser.test.ts
+++ tests/main/middlewares/body-parser.test.ts
@@ -2,14 +2,13 @@ import app from '@/main/config/app'
 
 import request from 'supertest'
 
+const agent = request.agent(app)
 describe('BodyParser', () => {
   beforeEach(() => {
     app.post('/use_body_parser', (req, res) => {
       res.send(req.body)
     })
   })
-
-  const agent = request.agent(app)
 
   it('Should parse body', async () => {
     await agent.post('/use_body_parser').send({ name: 'any_name' }).expect({ name: 'any_name' })
