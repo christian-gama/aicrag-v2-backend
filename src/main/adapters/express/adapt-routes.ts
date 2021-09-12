e3d0886f15385e3e8f44cafa7da74393f9b6d98f
Christian Gama <christiangama.dev@gmail.com>
2021-09-11 23:59:14 -0300

    feat: ensure errorHandler catches any error passed on next and return 500 with an error

--- src/main/adapters/express/adapt-routes.ts
+++ src/main/adapters/express/adapt-routes.ts
@@ -2,28 +2,39 @@ import { ControllerProtocol } from '@/presentation/controllers/protocols/control
 import { HttpRequest, HttpResponse } from '@/presentation/http/protocols'
 import { env } from '@/main/config/env'
 
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { InternalError } from '@/application/usecases/errors'
 
 export const adaptRoutes = (controller: ControllerProtocol) => {
-  return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = req
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const httpRequest: HttpRequest = req
 
-    const httpResponse: HttpResponse = await controller.handle(httpRequest)
+      const httpResponse: HttpResponse = await controller.handle(httpRequest)
 
-    if (httpResponse.accessToken) {
-      res.cookie('jwt', httpResponse.accessToken, {
-        maxAge: +env.COOKIES.EXPIRES * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: env.SERVER.NODE_ENV === 'production'
-      })
+      if (httpResponse.accessToken) {
+        res.cookie('jwt', httpResponse.accessToken, {
+          maxAge: +env.COOKIES.EXPIRES * 24 * 60 * 60 * 1000,
+          httpOnly: true,
+          secure: env.SERVER.NODE_ENV === 'production'
+        })
+      }
+
+      res.status(httpResponse.statusCode)
+
+      if (httpResponse.statusCode === 500 && env.SERVER.NODE_ENV === 'production') {
+        res.json({
+          status: httpResponse.status,
+          data: { message: new InternalError().message }
+        })
+      } else {
+        res.json({
+          status: httpResponse.status,
+          data: httpResponse.data
+        })
+      }
+    } catch (error) {
+      next(error)
     }
-
-    const jsonResponse = {
-      status: httpResponse.status,
-      data: httpResponse.data
-    }
-
-    res.status(httpResponse.statusCode)
-    res.json(jsonResponse)
   }
 }
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,13 @@
 import authenticationRoutes from '../routes/authentication-routes'
 
 import { Express } from 'express'
+import { errorHandler } from '../middlewares/error-handler'
 import { notFound } from '../middlewares/not-found'
 
 export default (app: Express): void => {
   if (process.env.NODE_ENV !== 'test') {
     app.use('/api/auth', authenticationRoutes)
     app.all('*', notFound)
+    app.use(errorHandler)
   }
 }
