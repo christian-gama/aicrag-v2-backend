e637ba8127491eba7d64c914cd8550a880c83143
Christian Gama <christiangama.dev@gmail.com>
2021-09-12 17:18:21 -0300

    test: ensure returns an AccountAlreadyActivatedError if account is already activated

--- tests/application/usecases/validators/codes/validate-activation-code.spec.ts
+++ tests/application/usecases/validators/codes/validate-activation-code.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidCodeError } from '@/application/usecases/errors'
+import { AccountAlreadyActivatedError, InvalidCodeError } from '@/application/usecases/errors'
 import { makeSut } from './__mocks__/codes/validate-activation-code-mock'
 
 describe('ValidateActivationCode', () => {
@@ -19,5 +19,16 @@ describe('ValidateActivationCode', () => {
     const value = await sut.validate(fakeData)
 
     expect(value).toEqual(new InvalidCodeError())
+  })
+
+  it('Should return an AccountAlreadyActivated if account is already activated', async () => {
+    const { sut, fakeUser } = makeSut()
+    fakeUser.settings.accountActivated = true
+
+    const fakeData = { email: fakeUser.personal.email, activationCode: 'any_code' }
+
+    const value = await sut.validate(fakeData)
+
+    expect(value).toEqual(new AccountAlreadyActivatedError())
   })
 })
