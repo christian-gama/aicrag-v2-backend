05a349c550ccfc8f3748ade9140fcfadb2427116
Christian Gama <christiangama.dev@gmail.com>
2021-09-12 21:49:43 -0300

    feat: ensure calls filter with correct use

--- src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
+++ src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
@@ -1,12 +1,15 @@
+import { User } from '@/domain/user'
 import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
+import { FilterUserDataProtocol } from '@/application/usecases/helpers/filter-user-data'
+import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
 import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helper/http/protocols'
-import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
 
 export class ActivateAccountController implements ControllerProtocol {
   constructor (
     private readonly accountDbRepository: AccountDbRepositoryProtocol,
     private readonly activateAccountValidator: ValidatorProtocol,
+    private readonly filterUserData: FilterUserDataProtocol,
     private readonly httpHelper: HttpHelperProtocol
   ) {}
 
@@ -17,7 +20,9 @@ export class ActivateAccountController implements ControllerProtocol {
 
     if (error) return this.httpHelper.unauthorized(error)
 
-    await this.accountDbRepository.findAccountByEmail(account.email)
+    const user = (await this.accountDbRepository.findAccountByEmail(account.email)) as User
+
+    this.filterUserData.filter(user)
 
     return Promise.resolve({ status: 'success', statusCode: 200, data: {} })
   }
