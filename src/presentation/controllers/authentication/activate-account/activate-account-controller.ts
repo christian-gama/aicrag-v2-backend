a17ec4e2335a885e39f2624e77cf622ed29f2aa7
Christian Gama <christiangama.dev@gmail.com>
2021-09-12 21:43:43 -0300

    feat: ensure calls findAccountByEmail with correct email

--- src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
+++ src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
@@ -1,9 +1,14 @@
-import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
+import { AccountDbRepositoryProtocol } from '@/application/protocols/repositories/account/account-db-repository-protocol'
 import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helper/http/protocols'
+import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
 
 export class ActivateAccountController implements ControllerProtocol {
-  constructor (private readonly activateAccountValidator: ValidatorProtocol, private readonly httpHelper: HttpHelperProtocol) {}
+  constructor (
+    private readonly accountDbRepository: AccountDbRepositoryProtocol,
+    private readonly activateAccountValidator: ValidatorProtocol,
+    private readonly httpHelper: HttpHelperProtocol
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const account = httpRequest.body
@@ -11,6 +16,8 @@ export class ActivateAccountController implements ControllerProtocol {
     const error = await this.activateAccountValidator.validate(account)
 
     if (error) return this.httpHelper.unauthorized(error)
+
+    await this.accountDbRepository.findAccountByEmail(account.email)
 
     return Promise.resolve({ status: 'success', statusCode: 200, data: {} })
   }
