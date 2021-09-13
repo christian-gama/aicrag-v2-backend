5ff9bd8cdad0642effa00249194671d51d575511
Christian Gama <christiangama.dev@gmail.com>
2021-09-12 21:38:21 -0300

    feat: ensure returns unauthorized if validation fails

--- src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
+++ src/presentation/controllers/authentication/activate-account/activate-account-controller.ts
@@ -1,14 +1,16 @@
 import { ValidatorProtocol } from '@/application/protocols/validators/validator-protocol'
 import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
-import { HttpRequest, HttpResponse } from '@/presentation/helper/http/protocols'
+import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helper/http/protocols'
 
 export class ActivateAccountController implements ControllerProtocol {
-  constructor (private readonly activateAccountValidator: ValidatorProtocol) {}
+  constructor (private readonly activateAccountValidator: ValidatorProtocol, private readonly httpHelper: HttpHelperProtocol) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const account = httpRequest.body
 
-    await this.activateAccountValidator.validate(account)
+    const error = await this.activateAccountValidator.validate(account)
+
+    if (error) return this.httpHelper.unauthorized(error)
 
     return Promise.resolve({ status: 'success', statusCode: 200, data: {} })
   }
