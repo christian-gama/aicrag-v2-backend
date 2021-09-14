ca7f97eca1ebc9b49d544d47897fe65b88acacff
Christian Gama <christiangama.dev@gmail.com>
2021-09-14 14:52:42 -0300

    test: ensure calls generate

--- tests/application/usecases/repositories/refresh-token/refresh-token-repository-sut.ts
+++ tests/application/usecases/repositories/refresh-token/refresh-token-repository-sut.ts
@@ -1,15 +1,19 @@
 import { User } from '@/domain/user'
 import { RefreshTokenRepository } from '@/application/usecases/repositories/refresh-token/refresh-token-repository'
+import { UuidProtocol } from '@/application/protocols/helpers/uuid/uuid-protocol'
 import { makeFakeUser } from '@/tests/__mocks__/domain/mock-user'
+import { makeUuidStub } from '@/tests/__mocks__/application/helpers/mock-uuid'
 
 interface SutTypes {
   sut: RefreshTokenRepository
   fakeUser: User
+  uuidStub: UuidProtocol
 }
 
 export const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
-  const sut = new RefreshTokenRepository()
+  const uuidStub = makeUuidStub()
+  const sut = new RefreshTokenRepository(uuidStub)
 
-  return { sut, fakeUser }
+  return { sut, fakeUser, uuidStub }
 }
--- tests/application/usecases/repositories/refresh-token/refresh-token-repository.spec.ts
+++ tests/application/usecases/repositories/refresh-token/refresh-token-repository.spec.ts
@@ -6,9 +6,18 @@ describe('RefreshTokenRepository', () => {
 
     const refreshToken = sut.createRefreshToken(fakeUser)
 
-    expect(refreshToken.id).toBe('any_id')
+    expect(refreshToken.id).toBe('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx')
     expect(typeof refreshToken.expiresIn).toBe('object')
     expect(refreshToken.user).toEqual(fakeUser)
     expect(refreshToken.userId).toBe(fakeUser.personal.id)
+  })
+
+  it('Should call generate', () => {
+    const { sut, fakeUser, uuidStub } = makeSut()
+    const generateSpy = jest.spyOn(uuidStub, 'generate')
+
+    sut.createRefreshToken(fakeUser)
+
+    expect(generateSpy).toHaveBeenCalled()
   })
 })
