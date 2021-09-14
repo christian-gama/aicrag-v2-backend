7a812089a631ffd30eada85b61d3705d0ed30268
Christian Gama <christiangama.dev@gmail.com>
2021-09-14 14:52:29 -0300

    feat: ensure calls generate

--- src/application/usecases/repositories/refresh-token/refresh-token-repository.ts
+++ src/application/usecases/repositories/refresh-token/refresh-token-repository.ts
@@ -1,11 +1,14 @@
+import { UuidProtocol } from '@/application/protocols/helpers/uuid/uuid-protocol'
 import { RefreshTokenRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-repository-protocol'
 import { RefreshToken } from '@/domain/refresh-token/refresh-token-protocol'
 import { User } from '@/domain/user'
 
 export class RefreshTokenRepository implements RefreshTokenRepositoryProtocol {
+  constructor (private readonly uuid: UuidProtocol) {}
+
   createRefreshToken (user: User): RefreshToken {
     return {
-      id: 'any_id',
+      id: this.uuid.generate(),
       expiresIn: new Date(),
       user: user,
       userId: user.personal.id
