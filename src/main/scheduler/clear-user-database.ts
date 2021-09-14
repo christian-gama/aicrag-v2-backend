62f15e2a3c1b5758dcf6af2e93a713b4058af2dc
Christian Gama <christiangama.dev@gmail.com>
2021-09-14 01:06:37 -0300

    feat: remove setInterval from file

--- src/main/scheduler/clear-user-database.ts
+++ src/main/scheduler/clear-user-database.ts
@@ -3,7 +3,6 @@ import { LogErrorProtocol } from '@/domain/log/log-error-protocol'
 import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
 import { UserDbFilter } from '@/infra/database/mongodb/user/protocols/update-user-options'
 import { FindOptions } from 'mongodb'
-import { makeLogErrorDbRepository } from '../factories/repositories/log/log-error-db-repository/log-error-db-repository-factory'
 
 export class ClearUserDatabase {
   constructor (private readonly logErrorDbRepository: LogErrorDbRepositoryProtocol) {}
@@ -21,7 +20,9 @@ export class ClearUserDatabase {
 
       const deleted = await userCollection.deleteMany(filter)
 
-      if (deleted.deletedCount > 0) { console.log(`ClearUserDatabase: ${deleted.deletedCount} inactive users were deleted.`) }
+      if (deleted.deletedCount > 0) {
+        console.log(`ClearUserDatabase: ${deleted.deletedCount} inactive users were deleted.`)
+      }
 
       return deleted.deletedCount
     } catch (error) {
@@ -29,9 +30,3 @@ export class ClearUserDatabase {
     }
   }
 }
-
-const clearUserDatabase = new ClearUserDatabase(makeLogErrorDbRepository())
-
-setInterval(async () => {
-  await clearUserDatabase.deleteInactiveUsers()
-}, 24 * 60 * 60 * 1000)
