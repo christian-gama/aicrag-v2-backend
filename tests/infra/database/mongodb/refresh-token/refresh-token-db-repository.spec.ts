e8de1431b7ab1dca364bf91b898dfbe61e0c0e39
Christian Gama <christiangama.dev@gmail.com>
2021-09-14 15:23:58 -0300

    test: ensure returns a RefreshToken from database

--- tests/infra/database/mongodb/refresh-token/refresh-token-db-repository.spec.ts
+++ tests/infra/database/mongodb/refresh-token/refresh-token-db-repository.spec.ts
@@ -1,6 +1,25 @@
+import { env } from '@/main/config/env'
+import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
 import { makeSut } from './refresh-token-db-repository-sut'
 
+import { Collection } from 'mongodb'
+
 describe('RefreshTokenDbRepository', () => {
+  let refreshTokenCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(env.DB.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    refreshTokenCollection = await MongoHelper.getCollection('users')
+    await refreshTokenCollection.deleteMany({})
+  })
+
   it('Should call createRefreshToken with correct user', async () => {
     const { sut, fakeUser, refreshTokenRepositoryStub } = makeSut()
     const createRefreshTokenSpy = jest.spyOn(refreshTokenRepositoryStub, 'createRefreshToken')
@@ -8,5 +27,17 @@ describe('RefreshTokenDbRepository', () => {
     await sut.saveRefreshToken(fakeUser)
 
     expect(createRefreshTokenSpy).toHaveBeenCalledWith(fakeUser)
+  })
+
+  it('Should save RefreshToken on database', async () => {
+    const { sut, fakeUser } = makeSut()
+
+    const refreshToken = await sut.saveRefreshToken(fakeUser)
+
+    expect(refreshToken).toHaveProperty('_id')
+    expect(refreshToken).toHaveProperty('id')
+    expect(refreshToken).toHaveProperty('expiresIn')
+    expect(refreshToken).toHaveProperty('user')
+    expect(refreshToken).toHaveProperty('userId')
   })
 })
