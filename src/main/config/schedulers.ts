00eacfdb92400b177291d9c995a0f79b52469a1b
Christian Gama <christiangama.dev@gmail.com>
2021-09-14 13:38:17 -0300

    feat: ensure does not run schedulers interval on tests

--- src/main/config/schedulers.ts
+++ src/main/config/schedulers.ts
@@ -1,9 +1,11 @@
 import { makeClearUserDatabase } from '../factories/schedulers/clear-user-database-factory'
 
 export default (): void => {
-  const clearUserDatabase = makeClearUserDatabase()
+  if (process.env.NODE_ENV !== 'test') {
+    const clearUserDatabase = makeClearUserDatabase()
 
-  setInterval(async () => {
-    await clearUserDatabase.deleteInactiveUsers()
-  }, 12 * 60 * 60 * 1000)
+    setInterval(async () => {
+      await clearUserDatabase.deleteInactiveUsers()
+    }, 12 * 60 * 60 * 1000)
+  }
 }
