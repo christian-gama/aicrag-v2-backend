9f3acc666fd8d642d92d910f569974528eef9028
Christian Gama <christiangama.dev@gmail.com>
2021-09-15 14:05:38 -0300

    refactor: change expiresIn from 15 days to 30 days

--- src/application/usecases/repositories/refresh-token/refresh-token-repository.ts
+++ src/application/usecases/repositories/refresh-token/refresh-token-repository.ts
@@ -1,6 +1,6 @@
-import { UuidProtocol } from '@/application/protocols/helpers/uuid/uuid-protocol'
-import { RefreshTokenRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-repository-protocol'
 import { RefreshToken } from '@/domain/refresh-token/refresh-token-protocol'
+import { RefreshTokenRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-repository-protocol'
+import { UuidProtocol } from '@/application/protocols/helpers/uuid/uuid-protocol'
 
 export class RefreshTokenRepository implements RefreshTokenRepositoryProtocol {
   constructor (private readonly uuid: UuidProtocol) {}
@@ -8,7 +8,8 @@ export class RefreshTokenRepository implements RefreshTokenRepositoryProtocol {
   createRefreshToken (userId: string): RefreshToken {
     return {
       id: this.uuid.generate(),
-      expiresIn: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
+      // expiresIn must have the same expiration from env.JWT.REFRESH_EXPIRES
+      expiresIn: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
       userId: userId
     }
   }
