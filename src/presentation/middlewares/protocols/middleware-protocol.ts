4e6b9da509a78111d2b169985fc36d6a71bc44cd
Christian Gama <christiangama.dev@gmail.com>
2021-09-15 14:24:45 -0300

    docs: add documentation to MiddlewareProtocol

--- src/presentation/middlewares/protocols/middleware-protocol.ts
+++ src/presentation/middlewares/protocols/middleware-protocol.ts
@@ -1,5 +1,12 @@
 import { HttpRequestToken, HttpResponse } from '@/presentation/helpers/http/protocols'
 
 export interface MiddlewareProtocol {
+  /**
+   * @async Asynchronous method.
+   * @description Generic middleware that receive a generic http request.
+   * @param httpRequest A generic http request that contains a token.
+   * @returns Return a generic http response.
+   */
+
   handle: (httpRequest: HttpRequestToken) => Promise<HttpResponse>
 }
