bca01b4cf57eb24e11d9aa08db65c5520987af20
Christian Gama <christiangama.dev@gmail.com>
2021-09-16 18:21:44 -0300

    feat: update user refreshToken property

--- src/infra/database/mongodb/refresh-token/refresh-token-db-repository.ts
+++ src/infra/database/mongodb/refresh-token/refresh-token-db-repository.ts
@@ -2,15 +2,20 @@ import { RefreshToken } from '@/domain/refresh-token/refresh-token-protocol'
 import { RefreshTokenDbRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-db-repository-protocol'
 import { RefreshTokenRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-repository-protocol'
 import { MongoHelper } from '../helper/mongo-helper'
+import { UserDbFilter } from '../user/protocols/update-user-options'
 export class RefreshTokenDbRepository implements RefreshTokenDbRepositoryProtocol {
-  constructor (
-    private readonly refreshTokenRepository: RefreshTokenRepositoryProtocol
-  ) {}
+  constructor (private readonly refreshTokenRepository: RefreshTokenRepositoryProtocol) {}
 
   async saveRefreshToken (userId: string): Promise<RefreshToken> {
     const refreshToken = await this.refreshTokenRepository.createRefreshToken(userId)
 
     const refreshTokenCollection = await MongoHelper.getCollection('refresh_tokens')
+
+    const userCollection = await MongoHelper.getCollection('users')
+
+    const userFilter: UserDbFilter = { 'personal.id': userId }
+    const userUpdate: UserDbFilter = { 'temporary.refreshToken': refreshToken.id }
+    await userCollection.updateOne(userFilter, { $set: userUpdate })
 
     const result = await refreshTokenCollection.insertOne(refreshToken)
 
