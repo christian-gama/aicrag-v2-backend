b36e1bb9f06e3d809f6aed06759ab9c0d7955312
Christian Gama <christiangama.dev@gmail.com>
2021-09-16 21:04:15 -0300

    feat: ensure the refresh token belongs to a user

--- src/main/factories/middlewares/authentication/refresh-token-middleware-factory.ts
+++ src/main/factories/middlewares/authentication/refresh-token-middleware-factory.ts
@@ -3,12 +3,16 @@ import { makeHttpHelper } from '@/main/factories/helpers/http-helper-factory'
 import { makeJwtAccessToken } from '@/main/factories/cryptography/jwt-access-token-factory'
 import { makeJwtRefreshToken } from '@/main/factories/cryptography/jwt-refresh-token-factory'
 import { makeRefreshTokenDbRepository } from '@/main/factories/repositories/refresh-token/refresh-token-db-repository/refresh-token-db-repository-factory'
+import { makeBcryptAdapter } from '../../cryptography/bcrypt-adapter-factory'
+import { makeUserDbRepository } from '../../repositories/user/user-db-repository/user-db-repository-factory'
 
 export const makeRefreshTokenMiddleware = (): RefreshTokenMiddleware => {
+  const comparer = makeBcryptAdapter()
   const httpHelper = makeHttpHelper()
   const jwtAccessToken = makeJwtAccessToken()
   const jwtRefreshToken = makeJwtRefreshToken()
   const refreshTokenDbRepository = makeRefreshTokenDbRepository()
+  const userDbRepository = makeUserDbRepository()
 
-  return new RefreshTokenMiddleware(httpHelper, jwtAccessToken, jwtRefreshToken, refreshTokenDbRepository)
+  return new RefreshTokenMiddleware(comparer, httpHelper, jwtAccessToken, jwtRefreshToken, refreshTokenDbRepository, userDbRepository)
 }
--- src/presentation/middlewares/authentication/refresh-token/refresh-token-middleware.ts
+++ src/presentation/middlewares/authentication/refresh-token/refresh-token-middleware.ts
@@ -1,7 +1,9 @@
+import { ComparerProtocol } from '@/application/protocols/cryptography'
 import { DecoderProtocol } from '@/application/protocols/cryptography/decoder-protocol'
 import { EncrypterProtocol } from '@/application/protocols/cryptography/encrypter-protocol'
 import { RefreshTokenDbRepositoryProtocol } from '@/application/protocols/repositories/refresh-token/refresh-token-db-repository-protocol'
-import { TokenMissingError } from '@/application/usecases/errors'
+import { UserDbRepositoryProtocol } from '@/application/protocols/repositories/user/user-db-repository-protocol'
+import { InvalidTokenError, TokenMissingError } from '@/application/usecases/errors'
 import {
   HttpHelperProtocol,
   HttpRequestToken,
@@ -10,10 +12,12 @@ import {
 import { MiddlewareProtocol } from '@/presentation/middlewares/protocols/middleware-protocol'
 export class RefreshTokenMiddleware implements MiddlewareProtocol {
   constructor (
+    private readonly comparer: ComparerProtocol,
     private readonly httpHelper: HttpHelperProtocol,
     private readonly jwtAccessToken: EncrypterProtocol,
     private readonly jwtRefreshToken: DecoderProtocol,
-    private readonly refreshTokenDbRepository: RefreshTokenDbRepositoryProtocol
+    private readonly refreshTokenDbRepository: RefreshTokenDbRepositoryProtocol,
+    private readonly userDbRepository: UserDbRepositoryProtocol
   ) {}
 
   async handle (httpRequest: HttpRequestToken): Promise<HttpResponse> {
@@ -30,6 +34,19 @@ export class RefreshTokenMiddleware implements MiddlewareProtocol {
     )
 
     if (!existentRefreshToken) return this.httpHelper.unauthorized(new TokenMissingError())
+
+    const refreshTokenOwner = await this.userDbRepository.findUserByRefreshToken(
+      existentRefreshToken.id
+    )
+
+    if (!refreshTokenOwner) return this.httpHelper.unauthorized(new TokenMissingError())
+
+    const isSameOwner = await this.comparer.compare(
+      refreshTokenOwner.personal.id,
+      existentRefreshToken.userId
+    )
+
+    if (!isSameOwner) return this.httpHelper.unauthorized(new InvalidTokenError())
 
     if (existentRefreshToken.expiresIn.getTime() < Date.now()) {
       await this.refreshTokenDbRepository.deleteRefreshTokenById(existentRefreshToken.userId)
