cf58016f36c9695c47493561b86428ea94371ca4
Christian Gama <christiangama.dev@gmail.com>
2021-09-19 20:54:21 -0300

    refactor: change passwords to uuid

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -7,15 +7,15 @@ dotenv.config({ path: path.resolve(__dirname, '../../../config.env') })
 export const env = {
   COOKIES: {
     EXPIRES: process.env.COOKIE_EXPIRES?.trim() ?? '30',
-    SECRET: process.env.COOKIE_SECRET?.trim() ?? '1X811PvLlkG8a9s1Elajlh142Z34YWum'
+    SECRET: process.env.COOKIE_SECRET?.trim() ?? 'ebcb6513-426a-4cac-ad4a-b768b6e12d59'
   },
   DB: {
     MONGO_URL: process.env.MONGO_URL?.trim() ?? 'mongodb://mongo/aicrag'
   },
   JWT: {
     ACCESS_EXPIRES: process.env.JWT_EXPIRES?.trim() ?? '10s',
-    ACCESS_SECRET: process.env.JWT_SECRET?.trim() ?? 'Ql818uILlkG8a8sdklajlm112Ac4yuuX',
-    REFRESH_SECRET: process.env.JWT_REFRESH_SECRET?.trim() ?? '2Z34YWnfoplk8a8sds1Elajlm81c431x',
+    ACCESS_SECRET: process.env.JWT_SECRET?.trim() ?? 'e26f414f-74cb-4f16-a1f1-db6438a1fcd6',
+    REFRESH_SECRET: process.env.JWT_REFRESH_SECRET?.trim() ?? '12f9b733-3ea0-4edf-9e05-33c1003dc9d1',
     REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES?.trim() ?? '30d'
   },
   SERVER: {
