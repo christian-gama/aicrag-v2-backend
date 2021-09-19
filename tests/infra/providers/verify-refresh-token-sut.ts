b4aebcc58a1454891dd6a038c2c9b81717d8bc6e
Christian Gama <christiangama.dev@gmail.com>
2021-09-19 14:58:57 -0300

    refactor: organize imports

--- tests/infra/providers/verify-refresh-token-sut.ts
+++ tests/infra/providers/verify-refresh-token-sut.ts
@@ -1,12 +1,12 @@
-import { IUser } from '@/domain/user'
 import { IRefreshToken } from '@/domain/refresh-token/refresh-token-protocol'
+import { IUser } from '@/domain/user'
 import { DecoderProtocol } from '@/application/protocols/cryptography/decoder-protocol'
 import { UserDbRepositoryProtocol } from '@/application/protocols/repositories/user/user-db-repository-protocol'
+import { VerifyRefreshToken } from '@/infra/providers/token/verify-refresh-token'
 import { makeFakeRefreshToken } from '@/tests/__mocks__/domain/mock-refresh-token'
 import { makeFakeUser } from '@/tests/__mocks__/domain/mock-user'
 import { makeDecoderStub } from '@/tests/__mocks__/infra/adapters/cryptography/mock-jwt-adapter'
 import { makeUserDbRepositoryStub } from '@/tests/__mocks__/infra/database/mongodb/user/mock-user-db-repository'
-import { VerifyRefreshToken } from '@/infra/providers/token/verify-refresh-token'
 
 interface SutTypes {
   sut: VerifyRefreshToken
