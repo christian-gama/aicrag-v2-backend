7626318e665cdcbea0baac3ae6166d7cfd2e70dc
Christian Gama <christiangama.dev@gmail.com>
2021-09-20 00:12:31 -0300

    feat: ensure calls updateUser with correct values

--- src/presentation/controllers/authentication/logout/logout-controller.ts
+++ src/presentation/controllers/authentication/logout/logout-controller.ts
@@ -1,12 +1,27 @@
 import { MustLoginError } from '@/application/usecases/errors'
-import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
+import { UserDbRepositoryProtocol } from '@/infra/database/mongodb/user'
+import { UserDbFilter } from '@/infra/database/mongodb/user/protocols/update-user-options'
+import {
+  HttpHelperProtocol,
+  HttpRequest,
+  HttpResponse
+} from '@/presentation/helpers/http/protocols'
 import { ControllerProtocol } from '../../protocols/controller-protocol'
 
 export class LogoutController implements ControllerProtocol {
-  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+  constructor (
+    private readonly httpHelper: HttpHelperProtocol,
+    private readonly userDbRepository: UserDbRepositoryProtocol
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.user) return this.httpHelper.forbidden(new MustLoginError())
+    const { user } = httpRequest
+
+    if (!user) return this.httpHelper.forbidden(new MustLoginError())
+
+    user.tokenVersion++
+    const updateFilter: UserDbFilter = { tokenVersion: user.tokenVersion }
+    await this.userDbRepository.updateUser(user, updateFilter)
 
     return this.httpHelper.ok({})
   }
