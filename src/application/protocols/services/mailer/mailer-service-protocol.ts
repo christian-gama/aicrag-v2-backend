f549d478b8d2b71451abd6e4cd719e6c21cece63
Christian Gama <christiangama.dev@gmail.com>
2021-09-21 20:07:53 -0300

    feat: create an interface for email service

---
+++ src/application/protocols/services/mailer/mailer-service-protocol.ts
@@ -0,0 +1,10 @@
+import { MailerSettingsProtocol } from './mailer-settings-protocol'
+
+export interface MailerServiceProtocol {
+  /**
+   * @description Receive the email settings and send it with through a transport.
+   * @param settings Settings that compose a email.
+   * @returns Return the mailer settings.
+   */
+  send: (settings: MailerSettingsProtocol) => Promise<MailerSettingsProtocol | Error>
+}
