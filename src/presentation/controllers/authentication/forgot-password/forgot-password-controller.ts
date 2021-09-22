29dff600e4d2152defcc92675215d2e6299b0673
Christian Gama <christiangama.dev@gmail.com>
2021-09-22 15:08:01 -0300

    feat: ensure calls updateUser with correct values

--- src/presentation/controllers/authentication/forgot-password/forgot-password-controller.ts
+++ src/presentation/controllers/authentication/forgot-password/forgot-password-controller.ts
@@ -24,12 +24,14 @@ export class ForgotPasswordController implements ControllerProtocol {
 
     if (error) return this.httpHelper.badRequest(error)
 
-    const user = await this.userDbRepository.findUserByEmail(email) as IUser
+    const user = (await this.userDbRepository.findUserByEmail(email)) as IUser
 
-    this.jwtAccessToken.encrypt({ email: user.personal.email, id: user.personal.id })
+    const resetPasswordToken = this.jwtAccessToken.encrypt({
+      email: user.personal.email,
+      id: user.personal.id
+    })
 
-    // TODO: Should update user temporary resetPasswordToken with the encrypted token
-    // TODO: Should send an email to the user with the encrypted token
+    await this.userDbRepository.updateUser(user, { 'temporary.resetPasswordToken': resetPasswordToken })
 
     return this.httpHelper.ok({})
   }
