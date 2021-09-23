b5c7a05b2e408cc65300d28896d5b1987d9d1f3b
Christian Gama <christiangama.dev@gmail.com>
2021-09-22 22:23:33 -0300

    test: use forgot-password route

---
+++ tests/integration/main/vendors/express/routes/authentication-routes/forgot-password.test.ts
@@ -0,0 +1,49 @@
+import { MongoHelper } from '@/infra/database/mongodb/helper/mongo-helper'
+import { forgotPasswordController } from '@/main/vendors/express/routes/.'
+import app from '@/main/vendors/express/config/app'
+import { makeFakeUser } from '@/tests/__mocks__/domain/mock-user'
+
+import { Collection } from 'mongodb'
+import request from 'supertest'
+
+describe('POST /forgot-password', () => {
+  let userCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+
+    userCollection = MongoHelper.getCollection('users')
+
+    app.post('/api/auth/forgot-password', forgotPasswordController)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  afterEach(async () => {
+    await userCollection.deleteMany({})
+  })
+
+  const agent = request.agent(app)
+
+  it('Should return 200 if all validations succeds', async () => {
+    const fakeUser = makeFakeUser()
+
+    await userCollection.insertOne(fakeUser)
+
+    await agent
+      .post('/api/auth/forgot-password')
+      .send({ email: fakeUser.personal.email })
+      .expect(200)
+  })
+
+  it('Should return 400 if email does not exist', async () => {
+    const fakeUser = makeFakeUser()
+
+    await agent
+      .post('/api/auth/forgot-password')
+      .send({ email: fakeUser.personal.email })
+      .expect(400)
+  })
+})
