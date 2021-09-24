5b7d5c3c2cb97130fa3a79ba5e873c29ad305d5a
Christian Gama <christiangama.dev@gmail.com>
2021-09-23 23:27:07 -0300

    fix: use jwtAccessToken instead of jwtRefreshToken

--- src/main/factories/providers/token/verify-reset-password-token-factory.ts
+++ src/main/factories/providers/token/verify-reset-password-token-factory.ts
@@ -1,10 +1,10 @@
 import { VerifyResetPasswordToken } from '@/infra/providers/token'
-import { makeJwtRefreshToken } from '../../cryptography'
+import { makeJwtAccessToken } from '../../cryptography'
 import { makeUserDbRepository } from '../../repositories'
 
 export const makeVerifyResetPasswordToken = (): VerifyResetPasswordToken => {
-  const jwtRefreshToken = makeJwtRefreshToken()
+  const jwtAccessToken = makeJwtAccessToken()
   const userDbRepository = makeUserDbRepository()
 
-  return new VerifyResetPasswordToken(jwtRefreshToken, userDbRepository)
+  return new VerifyResetPasswordToken(jwtAccessToken, userDbRepository)
 }
