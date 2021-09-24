2191989028ef28d3449607feb9c79b3c76b047f8
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 11:14:08 -0300

    feat: ensure returns unauthorized if verify fails

--- src/presentation/controllers/login/reset-password-controller.ts
+++ src/presentation/controllers/login/reset-password-controller.ts
@@ -1,16 +1,27 @@
 import { VerifyTokenProtocol } from '@/application/protocols/providers'
 import { MustLogoutError } from '@/application/usecases/errors'
-import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
+import {
+  HttpHelperProtocol,
+  HttpRequest,
+  HttpResponse
+} from '@/presentation/helpers/http/protocols'
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class ResetPasswordController implements ControllerProtocol {
-  constructor (private readonly httpHelper: HttpHelperProtocol, private readonly verifyResetPasswordToken: VerifyTokenProtocol) {}
+  constructor (
+    private readonly httpHelper: HttpHelperProtocol,
+
+    private readonly verifyResetPasswordToken: VerifyTokenProtocol
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (httpRequest.user) return this.httpHelper.forbidden(new MustLogoutError())
 
-    await this.verifyResetPasswordToken.verify(httpRequest.accessToken)
+    const response = await this.verifyResetPasswordToken.verify(httpRequest.accessToken)
 
+    if (response instanceof Error) {
+      return this.httpHelper.unauthorized(response)
+    }
     return this.httpHelper.ok({})
   }
 }
