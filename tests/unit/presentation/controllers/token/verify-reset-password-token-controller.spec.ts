e3cb0b415e01664223e096c119e0277f80c13e26
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 09:39:45 -0300

    test: ensure returns forbidden if user is logged in

--- tests/unit/presentation/controllers/token/verify-reset-password-token-controller.spec.ts
+++ tests/unit/presentation/controllers/token/verify-reset-password-token-controller.spec.ts
@@ -1,28 +1,53 @@
-import { VerifyTokenProtocol } from '@/application/protocols/providers'
-import { InvalidTokenError } from '@/application/usecases/errors'
+import { GenerateTokenProtocol, VerifyTokenProtocol } from '@/application/protocols/providers'
+import { InvalidTokenError, MustLogoutError } from '@/application/usecases/errors'
+import { IUser } from '@/domain'
 import { makeHttpHelper } from '@/main/factories/helpers'
 import { VerifyResetPasswordTokenController } from '@/presentation/controllers/token'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/protocols'
-import { makeVerifyTokenStub } from '@/tests/__mocks__'
+import { makeFakeUser, makeGenerateTokenStub, makeVerifyTokenStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   sut: VerifyResetPasswordTokenController
+  fakeUser: IUser
+  generateAccessTokenStub: GenerateTokenProtocol
   httpHelper: HttpHelperProtocol
   request: HttpRequest
   verifyResetPasswordTokenStub: VerifyTokenProtocol
 }
 
 const makeSut = (): SutTypes => {
+  const fakeUser = makeFakeUser()
+  const generateAccessTokenStub = makeGenerateTokenStub()
   const httpHelper = makeHttpHelper()
   const request = { params: { token: 'any_token' } }
   const verifyResetPasswordTokenStub = makeVerifyTokenStub()
 
-  const sut = new VerifyResetPasswordTokenController(httpHelper, verifyResetPasswordTokenStub)
+  const sut = new VerifyResetPasswordTokenController(
+    generateAccessTokenStub,
+    httpHelper,
+    verifyResetPasswordTokenStub
+  )
 
-  return { sut, httpHelper, request, verifyResetPasswordTokenStub }
+  return {
+    sut,
+    fakeUser,
+    generateAccessTokenStub,
+    httpHelper,
+    request,
+    verifyResetPasswordTokenStub
+  }
 }
 
-describe('VerifyTokenUrlController', () => {
+describe('VerifyResetPasswordTokenController', () => {
+  it('Should return forbidden if user is logged in', async () => {
+    const { sut, fakeUser, httpHelper, request } = makeSut()
+    request.user = fakeUser
+
+    const response = await sut.handle(request)
+
+    expect(response).toEqual(httpHelper.forbidden(new MustLogoutError()))
+  })
+
   it('Should call verify with correct values', async () => {
     const { sut, request, verifyResetPasswordTokenStub } = makeSut()
     const verifySpy = jest.spyOn(verifyResetPasswordTokenStub, 'verify')
@@ -34,11 +59,27 @@ describe('VerifyTokenUrlController', () => {
 
   it('Should return unauthorized if return an error', async () => {
     const { sut, httpHelper, request, verifyResetPasswordTokenStub } = makeSut()
-    jest.spyOn(verifyResetPasswordTokenStub, 'verify').mockReturnValueOnce(Promise.resolve(new InvalidTokenError()))
+    jest
+      .spyOn(verifyResetPasswordTokenStub, 'verify')
+      .mockReturnValueOnce(Promise.resolve(new InvalidTokenError()))
 
     const response = await sut.handle(request)
 
     expect(response).toEqual(httpHelper.unauthorized(new InvalidTokenError()))
+  })
+
+  it('Should call generate with correct user', async () => {
+    const { sut, fakeUser, generateAccessTokenStub, request, verifyResetPasswordTokenStub } =
+      makeSut()
+    jest
+      .spyOn(verifyResetPasswordTokenStub, 'verify')
+      .mockReturnValueOnce(Promise.resolve(fakeUser))
+
+    const generateSpy = jest.spyOn(generateAccessTokenStub, 'generate')
+
+    await sut.handle(request)
+
+    expect(generateSpy).toHaveBeenCalledWith(fakeUser)
   })
 
   it('Should return ok if finds a user', async () => {
@@ -46,6 +87,6 @@ describe('VerifyTokenUrlController', () => {
 
     const response = await sut.handle(request)
 
-    expect(response).toEqual(httpHelper.ok({ token: 'any_token' }))
+    expect(response).toEqual(httpHelper.ok({ accessToken: 'any_token' }))
   })
 })
