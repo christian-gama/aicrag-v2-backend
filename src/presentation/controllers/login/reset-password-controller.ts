8e2a95a52e5883041ccee0d6e2c1764f43626d83
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 10:34:56 -0300

    feat: ensure returns forbidden if user is logged in

---
+++ src/presentation/controllers/login/reset-password-controller.ts
@@ -0,0 +1,13 @@
+import { MustLogoutError } from '@/application/usecases/errors'
+import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
+import { ControllerProtocol } from '../protocols/controller-protocol'
+
+export class ResetPasswordController implements ControllerProtocol {
+  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (httpRequest.user) return this.httpHelper.forbidden(new MustLogoutError())
+
+    return this.httpHelper.ok({})
+  }
+}
