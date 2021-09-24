a728f10798422a7a975f0f42a6c189d541348586
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 11:05:11 -0300

    feat: ensure calls verify with correct token

--- src/presentation/controllers/login/reset-password-controller.ts
+++ src/presentation/controllers/login/reset-password-controller.ts
@@ -1,12 +1,15 @@
+import { VerifyTokenProtocol } from '@/application/protocols/providers'
 import { MustLogoutError } from '@/application/usecases/errors'
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class ResetPasswordController implements ControllerProtocol {
-  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+  constructor (private readonly httpHelper: HttpHelperProtocol, private readonly verifyAccessToken: VerifyTokenProtocol) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (httpRequest.user) return this.httpHelper.forbidden(new MustLogoutError())
+
+    await this.verifyAccessToken.verify(httpRequest.accessToken)
 
     return this.httpHelper.ok({})
   }
