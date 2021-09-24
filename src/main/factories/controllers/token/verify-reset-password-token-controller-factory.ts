08c7729717bd81a92072183fd45fd2b06bd57a0e
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 09:53:02 -0300

    fix: add missing dependency

--- src/main/factories/controllers/token/verify-reset-password-token-controller-factory.ts
+++ src/main/factories/controllers/token/verify-reset-password-token-controller-factory.ts
@@ -2,13 +2,15 @@ import { ControllerProtocol } from '@/presentation/controllers/protocols/control
 import { VerifyResetPasswordTokenController } from '@/presentation/controllers/token'
 import { makeTryCatchControllerDecorator } from '../../decorators'
 import { makeHttpHelper } from '../../helpers'
+import { makeGenerateAccessToken } from '../../providers/token'
 import { makeVerifyResetPasswordToken } from '../../providers/token/verify-reset-password-token-factory'
 
 export const makeVerifyResetPasswordTokenController = (): ControllerProtocol => {
+  const generateAccessToken = makeGenerateAccessToken()
   const httpHelper = makeHttpHelper()
   const verifyResetPasswordToken = makeVerifyResetPasswordToken()
 
-  const verifyTokenUrlController = new VerifyResetPasswordTokenController(httpHelper, verifyResetPasswordToken)
+  const verifyResetPasswordTokenController = new VerifyResetPasswordTokenController(generateAccessToken, httpHelper, verifyResetPasswordToken)
 
-  return makeTryCatchControllerDecorator(verifyTokenUrlController)
+  return makeTryCatchControllerDecorator(verifyResetPasswordTokenController)
 }
