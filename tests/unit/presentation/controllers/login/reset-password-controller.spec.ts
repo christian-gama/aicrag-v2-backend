f4d21392ab01660d53a059f712ef2ca7f4d5bd26
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 11:05:20 -0300

    test: ensure calls verify with correct token

--- tests/unit/presentation/controllers/login/reset-password-controller.spec.ts
+++ tests/unit/presentation/controllers/login/reset-password-controller.spec.ts
@@ -1,25 +1,28 @@
+import { VerifyTokenProtocol } from '@/application/protocols/providers'
 import { MustLogoutError } from '@/application/usecases/errors'
 import { IUser } from '@/domain'
 import { makeHttpHelper } from '@/main/factories/helpers'
 import { ResetPasswordController } from '@/presentation/controllers/login/reset-password-controller'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/protocols'
-import { makeFakeUser } from '@/tests/__mocks__'
+import { makeFakeUser, makeVerifyTokenStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   sut: ResetPasswordController
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
+  verifyAccessTokenStub: VerifyTokenProtocol
 }
 
 const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
   const httpHelper = makeHttpHelper()
-  const request = { body: { password: 'new_password', passwordConfirmation: 'new_password' } }
+  const request = { accessToken: 'any_token', body: { password: 'new_password', passwordConfirmation: 'new_password' } }
+  const verifyAccessTokenStub = makeVerifyTokenStub()
 
-  const sut = new ResetPasswordController(httpHelper)
+  const sut = new ResetPasswordController(httpHelper, verifyAccessTokenStub)
 
-  return { sut, fakeUser, httpHelper, request }
+  return { sut, fakeUser, httpHelper, request, verifyAccessTokenStub }
 }
 
 describe('ResetPasswordController', () => {
@@ -30,5 +33,14 @@ describe('ResetPasswordController', () => {
     const response = await sut.handle(request)
 
     expect(response).toEqual(httpHelper.forbidden(new MustLogoutError()))
+  })
+
+  it('Should call verify with correct token', async () => {
+    const { sut, request, verifyAccessTokenStub } = makeSut()
+    const verifyStub = jest.spyOn(verifyAccessTokenStub, 'verify')
+
+    await sut.handle(request)
+
+    expect(verifyStub).toHaveBeenCalledWith(request.accessToken)
   })
 })
