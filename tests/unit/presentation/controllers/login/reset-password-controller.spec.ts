81023bf4c8fa1eb9d3402d6c3a66f1ab1f27ae87
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 11:20:00 -0300

    test: ensure calls updateUser with correct values

--- tests/unit/presentation/controllers/login/reset-password-controller.spec.ts
+++ tests/unit/presentation/controllers/login/reset-password-controller.spec.ts
@@ -1,28 +1,38 @@
 import { VerifyTokenProtocol } from '@/application/protocols/providers'
+import { UserDbRepositoryProtocol } from '@/application/protocols/repositories'
 import { InvalidTokenError, MustLogoutError } from '@/application/usecases/errors'
 import { IUser } from '@/domain'
 import { makeHttpHelper } from '@/main/factories/helpers'
 import { ResetPasswordController } from '@/presentation/controllers/login/reset-password-controller'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/protocols'
-import { makeFakeUser, makeVerifyTokenStub } from '@/tests/__mocks__'
+import { makeFakeUser, makeUserDbRepositoryStub, makeVerifyTokenStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   sut: ResetPasswordController
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
+  userDbRepositoryStub: UserDbRepositoryProtocol
   verifyResetPasswordTokenStub: VerifyTokenProtocol
 }
 
 const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
   const httpHelper = makeHttpHelper()
-  const request = { accessToken: 'any_token', body: { password: 'new_password', passwordConfirmation: 'new_password' } }
+  const request = {
+    accessToken: 'any_token',
+    body: { password: 'new_password', passwordConfirmation: 'new_password' }
+  }
+  const userDbRepositoryStub = makeUserDbRepositoryStub(fakeUser)
   const verifyResetPasswordTokenStub = makeVerifyTokenStub()
 
-  const sut = new ResetPasswordController(httpHelper, verifyResetPasswordTokenStub)
+  const sut = new ResetPasswordController(
+    httpHelper,
+    userDbRepositoryStub,
+    verifyResetPasswordTokenStub
+  )
 
-  return { sut, fakeUser, httpHelper, request, verifyResetPasswordTokenStub }
+  return { sut, fakeUser, httpHelper, request, userDbRepositoryStub, verifyResetPasswordTokenStub }
 }
 
 describe('ResetPasswordController', () => {
@@ -46,10 +56,25 @@ describe('ResetPasswordController', () => {
 
   it('Should return unauthorized if verify fails', async () => {
     const { sut, httpHelper, request, verifyResetPasswordTokenStub } = makeSut()
-    jest.spyOn(verifyResetPasswordTokenStub, 'verify').mockReturnValueOnce(Promise.resolve(new InvalidTokenError()))
+    jest
+      .spyOn(verifyResetPasswordTokenStub, 'verify')
+      .mockReturnValueOnce(Promise.resolve(new InvalidTokenError()))
 
     const response = await sut.handle(request)
 
     expect(response).toEqual(httpHelper.unauthorized(new InvalidTokenError()))
+  })
+
+  it('Should call updateUser with correct values', async () => {
+    const { sut, fakeUser, request, userDbRepositoryStub, verifyResetPasswordTokenStub } = makeSut()
+    jest
+      .spyOn(verifyResetPasswordTokenStub, 'verify')
+      .mockReturnValueOnce(Promise.resolve(fakeUser))
+
+    const updateUserStub = jest.spyOn(userDbRepositoryStub, 'updateUser')
+
+    await sut.handle(request)
+
+    expect(updateUserStub).toHaveBeenCalledWith(fakeUser, { 'personal.password': request.body.password })
   })
 })
