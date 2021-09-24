4770cb74499e6182fac241d15780fe712f506945
Christian Gama <christiangama.dev@gmail.com>
2021-09-24 11:37:13 -0300

    feat: ensure calls hash with correct password

--- src/presentation/controllers/login/reset-password-controller.ts
+++ src/presentation/controllers/login/reset-password-controller.ts
@@ -1,3 +1,4 @@
+import { HasherProtocol } from '@/application/protocols/cryptography'
 import { VerifyTokenProtocol } from '@/application/protocols/providers'
 import { UserDbRepositoryProtocol } from '@/application/protocols/repositories'
 import { ValidatorProtocol } from '@/application/protocols/validators'
@@ -11,6 +12,7 @@ import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class ResetPasswordController implements ControllerProtocol {
   constructor (
+    private readonly hasher: HasherProtocol,
     private readonly httpHelper: HttpHelperProtocol,
     private readonly resetPasswordValidator: ValidatorProtocol,
     private readonly userDbRepository: UserDbRepositoryProtocol,
@@ -21,7 +23,6 @@ export class ResetPasswordController implements ControllerProtocol {
     if (httpRequest.user) return this.httpHelper.forbidden(new MustLogoutError())
 
     const response = await this.verifyResetPasswordToken.verify(httpRequest.accessToken)
-
     if (response instanceof Error) {
       return this.httpHelper.unauthorized(response)
     }
@@ -29,11 +30,12 @@ export class ResetPasswordController implements ControllerProtocol {
     const credentials = httpRequest.body
 
     const error = await this.resetPasswordValidator.validate(credentials)
-
     if (error) return this.httpHelper.badRequest(error)
 
+    const hashedPassword = await this.hasher.hash(credentials.password)
+
     await this.userDbRepository.updateUser(response, {
-      'personal.password': credentials.password
+      'personal.password': hashedPassword
     })
 
     return this.httpHelper.ok({})
