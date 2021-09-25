5fdb0a1587d23c5b9f86ee9fc2985cb835eb6dc4
Christian Gama <christiangama.dev@gmail.com>
2021-09-25 00:15:56 -0300

    refactor: move ResetPasswordController creation to a factory method

---
+++ src/main/factories/controllers/login/reset-password-controller-factory.ts
@@ -0,0 +1,32 @@
+import { ResetPasswordController } from '@/presentation/controllers/login/reset-password-controller'
+import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
+
+import { makeBcryptAdapter } from '../../cryptography'
+import { makeTryCatchControllerDecorator } from '../../decorators'
+import { makeFilterUserData, makeHttpHelper } from '../../helpers'
+import { makeGenerateRefreshToken } from '../../providers/token'
+import { makeVerifyResetPasswordToken } from '../../providers/token/verify-reset-password-token-factory'
+import { makeUserDbRepository } from '../../repositories'
+import { makeResetPasswordValidatorComposite } from '../../validators/reset-password-validator-composite-factory'
+
+export const makeResetPasswordController = (): ControllerProtocol => {
+  const filterUserData = makeFilterUserData()
+  const generateRefreshToken = makeGenerateRefreshToken()
+  const hasher = makeBcryptAdapter()
+  const httpHelper = makeHttpHelper()
+  const resetPasswordValidator = makeResetPasswordValidatorComposite()
+  const userDbRepository = makeUserDbRepository()
+  const verifyResetPasswordToken = makeVerifyResetPasswordToken()
+
+  const resetPasswordController = new ResetPasswordController(
+    filterUserData,
+    generateRefreshToken,
+    hasher,
+    httpHelper,
+    resetPasswordValidator,
+    userDbRepository,
+    verifyResetPasswordToken
+  )
+
+  return makeTryCatchControllerDecorator(resetPasswordController)
+}
