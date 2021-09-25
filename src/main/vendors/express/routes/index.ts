ab18066763bf923600bc21ebdb7bfa392057e798
Christian Gama <christiangama.dev@gmail.com>
2021-09-25 00:16:19 -0300

    feat: add resetPasswordController

--- src/main/vendors/express/routes/index.ts
+++ src/main/vendors/express/routes/index.ts
@@ -1,9 +1,6 @@
 import { makeLogoutController } from '@/main/factories/controllers/account'
-import {
-  makeLoginController,
-  makeActivateAccountController,
-  makeForgotPasswordController
-} from '@/main/factories/controllers/login'
+import { makeActivateAccountController, makeForgotPasswordController, makeLoginController } from '@/main/factories/controllers/login'
+import { makeResetPasswordController } from '@/main/factories/controllers/login/reset-password-controller-factory'
 import { makeSignUpController } from '@/main/factories/controllers/signup'
 import { makeVerifyResetPasswordTokenController } from '@/main/factories/controllers/token'
 import { makeAccessToken, makeRefreshToken } from '@/main/factories/middlewares/authentication'
@@ -21,6 +18,7 @@ export const logoutController = controllerAdapter(makeLogoutController())
 export const activateAccountController = controllerAdapter(makeActivateAccountController())
 export const forgotPasswordController = controllerAdapter(makeForgotPasswordController())
 export const loginController = controllerAdapter(makeLoginController())
+export const resetPasswordController = controllerAdapter(makeResetPasswordController())
 
 // SignUp Routes
 export const signUpController = controllerAdapter(makeSignUpController())
