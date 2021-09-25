a6be677b962043c76de5d621a218c88b8418877a
Christian Gama <christiangama.dev@gmail.com>
2021-09-25 00:16:38 -0300

    feat: add resetPasswordController route

--- src/main/vendors/express/routes/login-routes.ts
+++ src/main/vendors/express/routes/login-routes.ts
@@ -1,4 +1,10 @@
-import { loginController, accessTokenMiddleware, activateAccountController, forgotPasswordController } from '.'
+import {
+  loginController,
+  accessTokenMiddleware,
+  activateAccountController,
+  forgotPasswordController,
+  resetPasswordController
+} from '.'
 
 import { Router } from 'express'
 
@@ -7,5 +13,6 @@ const router = Router()
 router.post('/', loginController)
 router.post('/activate-account', accessTokenMiddleware, activateAccountController)
 router.post('/forgot-password', forgotPasswordController)
+router.post('/reset-password', resetPasswordController)
 
 export default router
