d8c0c8302591cd2f0910712e2039184788651290
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 15:10:36 -0300

    test: should call validate with correct credentials

--- tests/unit/presentation/controllers/account/update-password-controller.spec.ts
+++ tests/unit/presentation/controllers/account/update-password-controller.spec.ts
@@ -1,17 +1,20 @@
 import { IUser } from '@/domain'
 
+import { ValidatorProtocol } from '@/application/protocols/validators'
+
 import { UpdatePasswordController } from '@/presentation/controllers/account/update-password-controller'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/protocols'
 
 import { makeHttpHelper } from '@/main/factories/helpers'
 
-import { makeFakeUser } from '@/tests/__mocks__'
+import { makeFakeUser, makeValidatorStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
   sut: UpdatePasswordController
+  updatePasswordValidatorStub: ValidatorProtocol
 }
 
 const makeSut = (): SutTypes => {
@@ -20,13 +23,25 @@ const makeSut = (): SutTypes => {
   const request: HttpRequest = {
     body: { password: fakeUser.personal.password, passwordConfirmation: fakeUser.personal.password }
   }
+  const updatePasswordValidatorStub = makeValidatorStub()
 
-  const sut = new UpdatePasswordController(httpHelper)
+  const sut = new UpdatePasswordController(httpHelper, updatePasswordValidatorStub)
 
-  return { fakeUser, httpHelper, request, sut }
+  return { fakeUser, httpHelper, request, sut, updatePasswordValidatorStub }
 }
 
 describe('updatePasswordController', () => {
+  it('should call validate with correct credentials', async () => {
+    expect.hasAssertions()
+
+    const { request, sut, updatePasswordValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(updatePasswordValidatorStub, 'validate')
+
+    await sut.handle(request)
+
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+
   it('should return ok if succeds', async () => {
     expect.hasAssertions()
 
