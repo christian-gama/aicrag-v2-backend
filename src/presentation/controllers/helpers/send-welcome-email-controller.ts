7cef25da785b6d244444179628c96cc3a9e28dcd
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 00:23:13 -0300

    feat: ensure calls validate with correct credentials

--- src/presentation/controllers/helpers/send-welcome-email-controller.ts
+++ src/presentation/controllers/helpers/send-welcome-email-controller.ts
@@ -1,19 +1,23 @@
-import {
-  HttpHelperProtocol,
-  HttpRequest,
-  HttpResponse
-} from '@/presentation/helpers/http/protocols'
+import { ValidatorProtocol } from '@/application/protocols/validators'
+
+import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
 
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class SendWelcomeEmailController implements ControllerProtocol {
-  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+  constructor (
+    private readonly httpHelper: HttpHelperProtocol,
+    private readonly sendWelcomeValidator: ValidatorProtocol
+
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const email = httpRequest.body.email as string
+    const credentials = httpRequest.body
+
+    await this.sendWelcomeValidator.validate(credentials)
 
     return this.httpHelper.ok({
-      message: `A welcome email with activation code has been sent to ${email}`
+      message: `A welcome email with activation code has been sent to ${credentials.email as string}`
     })
   }
 }
