acaa4b82873d88adb464dc335017b3d4ce275a97
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 15:25:27 -0300

    test: should call hash with correct password

--- tests/unit/presentation/controllers/account/update-password-controller.spec.ts
+++ tests/unit/presentation/controllers/account/update-password-controller.spec.ts
@@ -1,5 +1,6 @@
 import { IUser } from '@/domain'
 
+import { HasherProtocol } from '@/application/protocols/cryptography'
 import { ValidatorProtocol } from '@/application/protocols/validators'
 
 import { UpdatePasswordController } from '@/presentation/controllers/account/update-password-controller'
@@ -7,10 +8,11 @@ import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/pro
 
 import { makeHttpHelper } from '@/main/factories/helpers'
 
-import { makeFakeUser, makeValidatorStub } from '@/tests/__mocks__'
+import { makeFakeUser, makeHasherStub, makeValidatorStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   fakeUser: IUser
+  hasherStub: HasherProtocol
   httpHelper: HttpHelperProtocol
   request: HttpRequest
   sut: UpdatePasswordController
@@ -19,15 +21,20 @@ interface SutTypes {
 
 const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
+  const hasherStub = makeHasherStub()
   const httpHelper = makeHttpHelper()
   const request: HttpRequest = {
-    body: { password: fakeUser.personal.password, passwordConfirmation: fakeUser.personal.password }
+    body: {
+      email: fakeUser.personal.email,
+      password: fakeUser.personal.password,
+      passwordConfirmation: fakeUser.personal.password
+    }
   }
   const updatePasswordValidatorStub = makeValidatorStub()
 
-  const sut = new UpdatePasswordController(httpHelper, updatePasswordValidatorStub)
+  const sut = new UpdatePasswordController(httpHelper, updatePasswordValidatorStub, hasherStub)
 
-  return { fakeUser, httpHelper, request, sut, updatePasswordValidatorStub }
+  return { fakeUser, hasherStub, httpHelper, request, sut, updatePasswordValidatorStub }
 }
 
 describe('updatePasswordController', () => {
@@ -46,11 +53,24 @@ describe('updatePasswordController', () => {
     expect.hasAssertions()
 
     const { httpHelper, request, sut, updatePasswordValidatorStub } = makeSut()
-    jest.spyOn(updatePasswordValidatorStub, 'validate').mockReturnValueOnce(Promise.resolve(new Error()))
+    jest
+      .spyOn(updatePasswordValidatorStub, 'validate')
+      .mockReturnValueOnce(Promise.resolve(new Error()))
 
     const response = await sut.handle(request)
 
     expect(response).toStrictEqual(httpHelper.badRequest(new Error()))
+  })
+
+  it('should call hash with correct password', async () => {
+    expect.hasAssertions()
+
+    const { hasherStub, request, sut } = makeSut()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+
+    await sut.handle(request)
+
+    expect(hashSpy).toHaveBeenCalledWith(request.body.password)
   })
 
   it('should return ok if succeds', async () => {
