f65402c78cb0568db54f121029d6cb2e9e83927a
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 00:23:23 -0300

    test: ensure calls validate with correct credentials

--- tests/unit/presentation/controllers/helpers/send-welcome-email-controller.spec.ts
+++ tests/unit/presentation/controllers/helpers/send-welcome-email-controller.spec.ts
@@ -1,35 +1,49 @@
 import { IUser } from '@/domain'
 
+import { ValidatorProtocol } from '@/application/protocols/validators'
+
 import { SendWelcomeEmailController } from '@/presentation/controllers/helpers/send-welcome-email-controller'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/helpers/http/protocols'
 
 import { makeHttpHelper } from '@/main/factories/helpers'
 
-import { makeFakeUser } from '@/tests/__mocks__'
+import { makeFakeUser, makeValidatorStub } from '@/tests/__mocks__'
 
 interface SutTypes {
   sut: SendWelcomeEmailController
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
+  sendWelcomeValidatorStub: ValidatorProtocol
 }
 
 const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
   const httpHelper = makeHttpHelper()
   const request: HttpRequest = { body: { email: fakeUser.personal.email } }
+  const sendWelcomeValidatorStub = makeValidatorStub()
 
-  const sut = new SendWelcomeEmailController(httpHelper)
+  const sut = new SendWelcomeEmailController(httpHelper, sendWelcomeValidatorStub)
 
   return {
     sut,
     fakeUser,
     httpHelper,
-    request
+    request,
+    sendWelcomeValidatorStub
   }
 }
 
 describe('SendWelcomeEmailController', () => {
+  it('Should call validate with correct credentials', async () => {
+    const { sut, request, sendWelcomeValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(sendWelcomeValidatorStub, 'validate')
+
+    await sut.handle(request)
+
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+
   it('Should return ok if send email', async () => {
     const { sut, fakeUser, httpHelper, request } = makeSut()
 
