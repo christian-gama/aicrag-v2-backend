ab7d047a22834dd1e527099d0274c800a2361f72
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 00:50:38 -0300

    test: ensure sendWelcomeEmailValidatorComposite is created with correct validators

---
+++ tests/integration/main/factories/validators/send-welcome-email-validator-composite.test.ts
@@ -0,0 +1,30 @@
+import { ValidatorProtocol } from '@/application/protocols/validators'
+import { ValidationComposite } from '@/application/usecases/validators'
+
+import {
+  makeRequiredFields,
+  makeSendWelcomeEmailValidatorComposite,
+  makeValidateEmail,
+  makeValidateEmailExists
+} from '@/main/factories/validators'
+
+jest.mock('../../../../../src/application/usecases/validators/validation-composite.ts')
+
+describe('CredentialsValidator Factory', () => {
+  it('Should create factory with all validations', () => {
+    makeSendWelcomeEmailValidatorComposite()
+
+    const validations: ValidatorProtocol[] = []
+
+    const fields = ['email']
+    for (const field of fields) {
+      validations.push(makeRequiredFields(field))
+    }
+
+    // Must have this exact validation order
+    validations.push(makeValidateEmail())
+    validations.push(makeValidateEmailExists())
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
