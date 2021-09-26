840764485484a746fdf5da18dbc88d72de41d97f
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 15:25:19 -0300

    feat: should call hash with correct password

--- src/presentation/controllers/account/update-password-controller.ts
+++ src/presentation/controllers/account/update-password-controller.ts
@@ -1,3 +1,4 @@
+import { HasherProtocol } from '@/application/protocols/cryptography'
 import { ValidatorProtocol } from '@/application/protocols/validators'
 
 import {
@@ -11,15 +12,17 @@ import { ControllerProtocol } from '../protocols/controller-protocol'
 export class UpdatePasswordController implements ControllerProtocol {
   constructor (
     private readonly httpHelper: HttpHelperProtocol,
-    private readonly updatePasswordValidator: ValidatorProtocol
+    private readonly updatePasswordValidator: ValidatorProtocol,
+    private readonly hasher: HasherProtocol
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const credentials = httpRequest.body
 
     const error = await this.updatePasswordValidator.validate(credentials)
-
     if (error) return this.httpHelper.badRequest(error)
+
+    await this.hasher.hash(credentials.password)
 
     return this.httpHelper.ok({ user: 'filteredUser' })
   }
