7406e89b8872aeb78d42acbaae214469c50841e4
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 00:24:43 -0300

    feat: ensure returns badRequest if validation fails

--- src/presentation/controllers/helpers/send-welcome-email-controller.ts
+++ src/presentation/controllers/helpers/send-welcome-email-controller.ts
@@ -1,6 +1,10 @@
 import { ValidatorProtocol } from '@/application/protocols/validators'
 
-import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/helpers/http/protocols'
+import {
+  HttpHelperProtocol,
+  HttpRequest,
+  HttpResponse
+} from '@/presentation/helpers/http/protocols'
 
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
@@ -8,16 +12,19 @@ export class SendWelcomeEmailController implements ControllerProtocol {
   constructor (
     private readonly httpHelper: HttpHelperProtocol,
     private readonly sendWelcomeValidator: ValidatorProtocol
-
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const credentials = httpRequest.body
 
-    await this.sendWelcomeValidator.validate(credentials)
+    const error = await this.sendWelcomeValidator.validate(credentials)
+
+    if (error) return this.httpHelper.badRequest(error)
 
     return this.httpHelper.ok({
-      message: `A welcome email with activation code has been sent to ${credentials.email as string}`
+      message: `A welcome email with activation code has been sent to ${
+        credentials.email as string
+      }`
     })
   }
 }
