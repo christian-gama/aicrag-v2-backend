d01613247a95faedba945b363c4b72e68734ceaf
Christian Gama <christiangama.dev@gmail.com>
2021-09-26 00:56:55 -0300

    feat: add partialProtectedMiddleware to send-welcome-email route

--- src/main/vendors/express/routes/helpers-routes.ts
+++ src/main/vendors/express/routes/helpers-routes.ts
@@ -1,10 +1,10 @@
-import { sendWelcomeEmailController, verifyResetPasswordTokenController } from '.'
+import { partialProtectedMiddleware, sendWelcomeEmailController, verifyResetPasswordTokenController } from '.'
 
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('/verify-reset-password-token/:token', verifyResetPasswordTokenController)
-router.post('/send-welcome-email/', sendWelcomeEmailController)
+router.post('/send-welcome-email/', partialProtectedMiddleware, sendWelcomeEmailController)
 
 export default router
