755f6c4fc25063fbd5e1abb1f1f8b8d3c924d7b6
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 09:15:07 -0300

    refactor: move SendEmailCodeValidatorComposite creation to a factory method

---
+++ src/main/factories/validators/send-email-code-validator-composite.ts
@@ -0,0 +1,22 @@
+import { ValidatorProtocol } from '@/application/protocols/validators'
+import { ValidationComposite } from '@/application/usecases/validators'
+
+import { makeRequiredFields, makeValidateEmail, makeValidateEmailExists, makeValidateTempEmail } from '.'
+
+export const makeSendEmailCodeValidatorComposite = (): ValidatorProtocol => {
+  const validations: ValidatorProtocol[] = []
+
+  const fields = ['email']
+  for (const field of fields) {
+    validations.push(makeRequiredFields(field))
+  }
+
+  // Must have this exact validation order
+  validations.push(makeValidateEmail())
+  validations.push(makeValidateEmailExists())
+  validations.push(makeValidateTempEmail())
+
+  const sendEmailCodeValidatorComposite = new ValidationComposite(validations)
+
+  return sendEmailCodeValidatorComposite
+}
