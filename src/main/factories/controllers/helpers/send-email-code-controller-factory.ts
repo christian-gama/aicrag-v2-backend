5c89e3d8fd9e23987d1fb4243c9fad030deffa2c
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 09:17:13 -0300

    refactor: move SendEmailCodeController creation to a factory method

---
+++ src/main/factories/controllers/helpers/send-email-code-controller-factory.ts
@@ -0,0 +1,24 @@
+import { SendEmailCodeController } from '@/presentation/controllers/helpers'
+import { ControllerProtocol } from '@/presentation/controllers/protocols/controller-protocol'
+
+import { makeTryCatchDecorator } from '../../decorators'
+import { makeHttpHelper } from '../../helpers'
+import { makeEmailCode } from '../../mailer'
+import { makeUserDbRepository } from '../../repositories'
+import { makeSendEmailCodeValidatorComposite } from '../../validators'
+
+export const makeSendEmailCodeController = (): ControllerProtocol => {
+  const httpHelper = makeHttpHelper()
+  const sendEmailCodeValidator = makeSendEmailCodeValidatorComposite()
+  const userDbRepository = makeUserDbRepository()
+  const emailCode = makeEmailCode()
+
+  const sendEmailCodeController = new SendEmailCodeController(
+    emailCode,
+    httpHelper,
+    sendEmailCodeValidator,
+    userDbRepository
+  )
+
+  return makeTryCatchDecorator(sendEmailCodeController)
+}
--- src/main/factories/mailer/index.ts
+++ src/main/factories/mailer/index.ts
@@ -1,2 +1,3 @@
+export * from './email-code-factory'
 export * from './forgot-password-email-factory'
 export * from './welcome-email-factory'
