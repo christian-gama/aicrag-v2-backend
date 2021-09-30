74277c8290dfa30bc113d65c4e282fa54f4eec76
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 08:40:20 -0300

    feat: validate temporary email requirements

---
+++ src/application/usecases/validators/validate-temp-email.ts
@@ -0,0 +1,18 @@
+import { UserDbRepositoryProtocol } from '@/application/protocols/repositories'
+import { ValidatorProtocol } from '@/application/protocols/validators'
+
+import { UserCredentialError } from '../errors'
+
+export class ValidateTempEmail implements ValidatorProtocol {
+  constructor (private readonly userDbRepository: UserDbRepositoryProtocol) {}
+
+  async validate (input: any): Promise<UserCredentialError | undefined> {
+    const { email } = input
+
+    const user = await this.userDbRepository.findUserByEmail(email)
+
+    if (user == null) return new UserCredentialError()
+
+    if (!user.temporary.tempEmail) return new UserCredentialError()
+  }
+}
