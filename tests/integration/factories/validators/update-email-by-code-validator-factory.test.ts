54ed7c6098ff3d9ff71e8f6f6e9fe11f20e3141b
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 18:37:32 -0300

    test: ensure updateEmailByCodeValidatorComposite creation is correct

---
+++ tests/integration/factories/validators/update-email-by-code-validator-factory.test.ts
@@ -0,0 +1,30 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { ValidationComposite } from '@/application/validators'
+
+import {
+  makeUpdateEmailByCodeValidatorComposite,
+  makeRequiredFields,
+  makeValidateEmailCode
+} from '@/factories/validators'
+
+jest.mock('../../../../src/application/validators/validation-composite.ts')
+
+describe('updateEmailByCodeValidatorComposite', () => {
+  it('should create factory with all validations', () => {
+    expect.hasAssertions()
+
+    makeUpdateEmailByCodeValidatorComposite()
+
+    const validations: ValidatorProtocol[] = []
+
+    const fields = ['email', 'tempEmailCode']
+    for (const field of fields) {
+      validations.push(makeRequiredFields(field))
+    }
+
+    validations.push(makeValidateEmailCode())
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
