b9f10a798f77291d9e6f9dff47109922b138dffa
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 17:16:22 -0300

    refactor: move UpdateEmailByCodeValidatorComposite  creation to a factory method

---
+++ src/factories/validators/update-email-by-code-validator-composite-factory.ts
@@ -0,0 +1,20 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { ValidationComposite } from '@/application/validators'
+
+import { makeRequiredFields, makeValidateEmailCode } from '.'
+
+export const makeUpdateEmailByCodeValidatorComposite = (): ValidatorProtocol => {
+  const validations: ValidatorProtocol[] = []
+
+  const fields = ['email', 'activationCode']
+  for (const field of fields) {
+    validations.push(makeRequiredFields(field))
+  }
+
+  validations.push(makeValidateEmailCode())
+
+  const updateEmailByCodeController = new ValidationComposite(validations)
+
+  return updateEmailByCodeController
+}
