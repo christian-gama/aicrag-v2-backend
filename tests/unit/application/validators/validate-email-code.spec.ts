facec775144e2823e501ab1ac1bca7cb7250baf2
Christian Gama <christiangama.dev@gmail.com>
2021-09-30 08:40:46 -0300

    test: validate temporary email requirements

---
+++ tests/unit/application/validators/validate-email-code.spec.ts
@@ -0,0 +1,113 @@
+import { IUser } from '@/domain'
+
+import { UserDbRepositoryProtocol } from '@/application/protocols/repositories'
+import { InvalidCodeError, CodeIsExpiredError } from '@/application/usecases/errors'
+import { ValidateEmailCode } from '@/application/usecases/validators'
+
+import { HttpRequest } from '@/presentation/helpers/http/protocols'
+
+import { makeFakeUser, makeUserDbRepositoryStub } from '@/tests/__mocks__'
+
+interface SutTypes {
+  fakeUser: IUser
+  request: HttpRequest
+  sut: ValidateEmailCode
+  userDbRepositoryStub: UserDbRepositoryProtocol
+}
+
+const makeSut = (): SutTypes => {
+  const fakeUser = makeFakeUser()
+  fakeUser.temporary.tempEmail = 'any_email@mail.com'
+  fakeUser.temporary.tempEmailCode = 'abcde'
+  fakeUser.temporary.tempEmailCodeExpiration = new Date(Date.now() + 10 * 60 * 1000)
+  const request: HttpRequest = {
+    body: {
+      email: fakeUser.personal.email,
+      tempEmailCode: fakeUser.temporary.tempEmailCode
+    }
+  }
+  const userDbRepositoryStub = makeUserDbRepositoryStub(fakeUser)
+
+  const sut = new ValidateEmailCode(userDbRepositoryStub)
+
+  return { fakeUser, request, sut, userDbRepositoryStub }
+}
+
+describe('validateEmailCode', () => {
+  it('should call findUserByEmail with correct email', async () => {
+    expect.hasAssertions()
+
+    const { request, sut, userDbRepositoryStub } = makeSut()
+    const findUserByEmailSpy = jest.spyOn(userDbRepositoryStub, 'findUserByEmail')
+
+    await sut.validate(request.body)
+
+    expect(findUserByEmailSpy).toHaveBeenCalledWith(request.body.email)
+  })
+
+  it('should return an InvalidCodeError if email code is not valid', async () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+    request.body.tempEmailCode = null
+
+    const value = await sut.validate(request.body)
+
+    expect(value).toStrictEqual(new InvalidCodeError())
+  })
+
+  it('should return an InvalidCode if there is no temporary email', async () => {
+    expect.hasAssertions()
+
+    const { fakeUser, request, sut, userDbRepositoryStub } = makeSut()
+    fakeUser.temporary.tempEmail = null
+    jest
+      .spyOn(userDbRepositoryStub, 'findUserByEmail')
+      .mockReturnValueOnce(Promise.resolve(fakeUser))
+
+    const value = await sut.validate(request.body)
+
+    expect(value).toStrictEqual(new InvalidCodeError())
+  })
+
+  it('should return a CodeIsExpiredError if temporary email code is expired', async () => {
+    expect.hasAssertions()
+
+    const { fakeUser, request, sut, userDbRepositoryStub } = makeSut()
+    fakeUser.temporary.tempEmailCodeExpiration = new Date(Date.now() - 1000)
+    jest
+      .spyOn(userDbRepositoryStub, 'findUserByEmail')
+      .mockReturnValueOnce(Promise.resolve(fakeUser))
+
+    const value = await sut.validate(request.body)
+
+    expect(value).toStrictEqual(new CodeIsExpiredError())
+  })
+
+  it('should return an InvalidCodeError if there is no tempCodeExpiration', async () => {
+    expect.hasAssertions()
+
+    const { fakeUser, request, sut, userDbRepositoryStub } = makeSut()
+    fakeUser.temporary.tempEmailCodeExpiration = null
+    jest
+      .spyOn(userDbRepositoryStub, 'findUserByEmail')
+      .mockReturnValueOnce(Promise.resolve(fakeUser))
+
+    const value = await sut.validate(request.body)
+
+    expect(value).toStrictEqual(new InvalidCodeError())
+  })
+
+  it('should return an InvalidCodeError if there is no user', async () => {
+    expect.hasAssertions()
+
+    const { request, sut, userDbRepositoryStub } = makeSut()
+    jest
+      .spyOn(userDbRepositoryStub, 'findUserByEmail')
+      .mockReturnValueOnce(Promise.resolve(undefined))
+
+    const value = await sut.validate(request.body)
+
+    expect(value).toStrictEqual(new InvalidCodeError())
+  })
+})
---
+++ tests/unit/application/validators/validate-temp-email.spec.ts
@@ -0,0 +1,74 @@
+import { IUser } from '@/domain'
+
+import { UserDbRepositoryProtocol } from '@/application/protocols/repositories'
+import { UserCredentialError } from '@/application/usecases/errors'
+import { ValidateTempEmail } from '@/application/usecases/validators'
+
+import { makeFakeUser, makeUserDbRepositoryStub } from '@/tests/__mocks__'
+
+interface SutTypes {
+  fakeUser: IUser
+  sut: ValidateTempEmail
+  userDbRepositoryStub: UserDbRepositoryProtocol
+}
+
+const makeSut = (): SutTypes => {
+  const fakeUser = makeFakeUser()
+  const userDbRepositoryStub = makeUserDbRepositoryStub(fakeUser)
+
+  const sut = new ValidateTempEmail(userDbRepositoryStub)
+
+  return { fakeUser, sut, userDbRepositoryStub }
+}
+
+describe('validateTempEmail', () => {
+  it('should return a UserCredentialError if email does not exists', async () => {
+    expect.hasAssertions()
+
+    const { sut, userDbRepositoryStub } = makeSut()
+    const credentials = { email: 'invalid_email@email.com', password: 'any_password' }
+    jest
+      .spyOn(userDbRepositoryStub, 'findUserByEmail')
+      .mockReturnValueOnce(Promise.resolve(undefined))
+
+    const error = await sut.validate(credentials)
+
+    expect(error).toStrictEqual(new UserCredentialError())
+  })
+
+  it('should return a UserCredentialError if temporary email does not exists', async () => {
+    expect.hasAssertions()
+
+    const { fakeUser, sut } = makeSut()
+    const credentials = { email: 'invalid_email@email.com' }
+    fakeUser.temporary.tempEmail = null
+
+    const error = await sut.validate(credentials)
+
+    expect(error).toStrictEqual(new UserCredentialError())
+  })
+
+  it('should return a undefined if temporary email exist', async () => {
+    expect.hasAssertions()
+
+    const { fakeUser, sut } = makeSut()
+    const credentials = { email: 'invalid_email@email.com' }
+    fakeUser.temporary.tempEmail = 'any_email@mail.com'
+
+    const response = await sut.validate(credentials)
+
+    expect(response).toBeUndefined()
+  })
+
+  it('should call findUserByEmail with correct value', async () => {
+    expect.hasAssertions()
+
+    const { sut, userDbRepositoryStub } = makeSut()
+    const credentials = { email: 'invalid_email@email.com', password: 'any_password' }
+    const findUserByEmailSpy = jest.spyOn(userDbRepositoryStub, 'findUserByEmail')
+
+    await sut.validate(credentials)
+
+    expect(findUserByEmailSpy).toHaveBeenCalledWith(credentials.email)
+  })
+})
