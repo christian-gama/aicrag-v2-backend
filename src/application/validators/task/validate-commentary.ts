c6e04c1288a2e1e315d921560b0af7e1b7b9ccca
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 20:02:59 -0300

    feat: ensure return undefined if commentary is null

--- src/application/validators/task/validate-commentary.ts
+++ src/application/validators/task/validate-commentary.ts
@@ -4,6 +4,8 @@ import { InvalidParamError } from '@/application/errors'
 
 export class ValidateCommentary implements ValidatorProtocol {
   validate (input: any): InvalidParamError | undefined {
+    if (input.commentary == null) return
+
     if (input.commentary.length > 400) return new InvalidParamError('commentary')
   }
 }
