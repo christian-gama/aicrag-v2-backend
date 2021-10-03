c72ec5ed11ebe25f606b1bfd99f059ece870baf4
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 18:09:58 -0300

    refactor: add undefined as return to FindByIdDbProtocol

--- src/domain/repositories/task/task-db-repository-protocol.ts
+++ src/domain/repositories/task/task-db-repository-protocol.ts
@@ -3,7 +3,7 @@ import { ITask, ITaskData, IUser } from '@/domain'
 export interface TaskDbRepositoryProtocol extends FindTaskByIdDbProtocol, SaveTaskDbProtocol {}
 
 export interface FindTaskByIdDbProtocol {
-  findTaskById: (id: string, user: IUser) => Promise<ITask>
+  findTaskById: (id: string, user: IUser) => Promise<ITask | undefined>
 }
 
 export interface SaveTaskDbProtocol {
