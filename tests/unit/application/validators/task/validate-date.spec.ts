3fcc915b286c3874f5891716403e7a5ea0309ccf
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 18:51:39 -0300

    test: ensure return InvalidParamError if is an invalid date

---
+++ tests/unit/application/validators/task/validate-date.spec.ts
@@ -0,0 +1,29 @@
+import { InvalidParamError } from '@/application/errors'
+import { ValidateDate } from '@/application/validators/task'
+
+import { HttpRequest } from '@/presentation/http/protocols'
+
+interface SutTypes {
+  request: HttpRequest
+  sut: ValidateDate
+}
+const makeSut = (): SutTypes => {
+  const request: HttpRequest = { body: { date: new Date(Date.now()) } }
+
+  const sut = new ValidateDate()
+
+  return { request, sut }
+}
+
+describe('validateDate', () => {
+  it('should return InvalidParamError if is an invalid date', () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+    request.body.date = 'invalid_date'
+
+    const error = sut.validate(request.body)
+
+    expect(error).toStrictEqual(new InvalidParamError('date'))
+  })
+})
