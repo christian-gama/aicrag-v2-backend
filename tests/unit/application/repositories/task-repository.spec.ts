1f085d17d7ceede2545ec28bfc4c83344a57100f
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 12:10:28 -0300

    test: ensure createTask returns a task

---
+++ tests/unit/application/repositories/task-repository.spec.ts
@@ -0,0 +1,64 @@
+import { ITaskData } from '@/domain'
+import { UuidProtocol } from '@/domain/helpers'
+
+import { TaskRepository } from '@/application/repositories'
+
+import { makeUuidStub } from '@/tests/__mocks__'
+import { makeFakeTaskData } from '@/tests/__mocks__/mock-task'
+
+import MockDate from 'mockdate'
+
+interface SutTypes {
+  fakeTaskData: ITaskData
+  sut: TaskRepository
+  uuidStub: UuidProtocol
+}
+
+const makeSut = (): SutTypes => {
+  const fakeTaskData = makeFakeTaskData()
+  const uuidStub = makeUuidStub()
+
+  const sut = new TaskRepository(uuidStub)
+
+  return { fakeTaskData, sut, uuidStub }
+}
+
+describe('taskRepository', () => {
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  it('should return a task with correct values', async () => {
+    expect.hasAssertions()
+
+    const { fakeTaskData, sut, uuidStub } = makeSut()
+    const { commentary, date, duration, taskId, type, user } = fakeTaskData
+    const fakeId = uuidStub.generate()
+
+    const task = sut.createTask(fakeTaskData)
+
+    expect(task).toStrictEqual({
+      commentary,
+      date: {
+        day: date.getDate(),
+        full: date,
+        hours: date.toLocaleTimeString(),
+        month: date.getMonth(),
+        year: date.getFullYear()
+      },
+      duration,
+      id: fakeId,
+      logs: {
+        createdAt: new Date(Date.now()),
+        updatedAt: null
+      },
+      taskId,
+      type,
+      user
+    })
+  })
+})
