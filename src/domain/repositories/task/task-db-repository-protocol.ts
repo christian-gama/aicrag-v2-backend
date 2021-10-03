38ea6b2a30314fac3fcbac6f50fc64d746b618e3
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 17:42:21 -0300

    feat: add FindTaskById protocol

--- src/domain/repositories/task/task-db-repository-protocol.ts
+++ src/domain/repositories/task/task-db-repository-protocol.ts
@@ -1,6 +1,10 @@
 import { ITask, ITaskData } from '@/domain'
 
-export interface TaskDbRepositoryProtocol extends SaveTaskDbProtocol {}
+export interface TaskDbRepositoryProtocol extends FindTaskByIdDbProtocol, SaveTaskDbProtocol {}
+
+export interface FindTaskByIdDbProtocol {
+  saveTask: (taskData: ITaskData) => Promise<ITask>
+}
 
 export interface SaveTaskDbProtocol {
   saveTask: (taskData: ITaskData) => Promise<ITask>
