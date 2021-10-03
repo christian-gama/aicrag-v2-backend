88263954434a6d29cfb5c4d9083680d417408c1b
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 18:40:10 -0300

    refactor: remove useless async

--- src/application/validators/task/validate-task-id.ts
+++ src/application/validators/task/validate-task-id.ts
@@ -3,7 +3,7 @@ import { ValidatorProtocol } from '@/domain/validators'
 import { InvalidParamError } from '@/application/errors'
 
 export class ValidateTaskId implements ValidatorProtocol {
-  async validate (input: any): Promise<InvalidParamError | undefined> {
+  validate (input: any): InvalidParamError | undefined {
     if (input.taskId == null) return
 
     if (input.taskId.length > 120) return new InvalidParamError('taskId')
