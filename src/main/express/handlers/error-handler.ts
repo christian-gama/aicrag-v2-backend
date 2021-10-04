1a099f9e16321387f674583ec1032a0ee1124c5c
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 22:50:34 -0300

    refactor: remove unreachable code

--- src/main/express/handlers/error-handler.ts
+++ src/main/express/handlers/error-handler.ts
@@ -1,18 +1,11 @@
-import { InvalidTokenError } from '@/application/errors'
+import { InternalError } from '@/application/errors'
 
 export const errorHandler = (err: Error): any => {
   let error = Object.create(err)
 
   switch (error.name) {
-    case 'JsonWebTokenError':
-      error = new InvalidTokenError()
-      error.statusCode = 401
-      break
-    case 'TokenExpiredError':
-      error = new InvalidTokenError()
-      error.statusCode = 401
-      break
     default:
+      error = new InternalError()
       error.statusCode = 500
   }
 
--- src/main/express/middlewares/error-request-handler.ts
+++ src/main/express/middlewares/error-request-handler.ts
@@ -9,33 +9,15 @@ export const errorRequestHandler: ErrorRequestHandler = (err, req, res, next) =>
 
   // Development
   if (environment.SERVER.NODE_ENV === 'development') {
-    if (error.statusCode.toString().startsWith('4')) {
-      res.status(error.statusCode)
-      return res.json({
-        data: { error: { message: error.message, name: error.name, stack: error.stack } },
-        status: 'fail'
-      })
-    }
-
-    res.status(500)
-    return res.json({
+    return res.status(error.statusCode).json({
       data: { error: { message: error.message, name: error.name, stack: error.stack } },
       status: 'fail'
     })
   }
 
   // Production
-  if (error.statusCode.toString().startsWith('4')) {
-    res.status(error.statusCode)
-    return res.json({
-      data: { message: error.message },
-      status: 'fail'
-    })
-  }
-
-  res.status(500)
-  return res.json({
-    data: { message: 'Internal error: Try again later' },
+  return res.status(error.statusCode).json({
+    data: { error: { message: error.message } },
     status: 'fail'
   })
 }
