9d84339bfdc12861f3c2036cf4527d8ddccd97ec
Christian Gama <christiangama.dev@gmail.com>
2021-10-03 21:02:46 -0300

    test: ensure CreateTaskValidatorComposite create validations with correct order

---
+++ tests/integration/factories/validators/create-task-validator-composite-factory.test.ts
@@ -0,0 +1,35 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { ValidationComposite } from '@/application/validators/validation-composite'
+
+import { makeValidateUniqueTaskId, makeValidateType, makeValidateDate, makeValidateDuration, makeValidateStatus, makeValidateTaskId, makeCreateTaskValidatorComposite } from '@/factories/validators/task'
+import { makeValidateCommentary } from '@/factories/validators/task/validate-commentary-factory'
+import { makeRequiredFields } from '@/factories/validators/validate-required-fields-factory'
+
+jest.mock('../../../../src/application/validators/validation-composite.ts')
+
+describe('createTaskValidatorComposite', () => {
+  it('should create factory with all validations', () => {
+    expect.hasAssertions()
+
+    makeCreateTaskValidatorComposite()
+
+    const validations: ValidatorProtocol[] = []
+
+    const fields = ['commentary', 'date', 'duration', 'status', 'taskId', 'type']
+    for (const field of fields) {
+      validations.push(makeRequiredFields(field))
+    }
+
+    // Must have this exact validation order
+    validations.push(makeValidateUniqueTaskId())
+    validations.push(makeValidateCommentary())
+    validations.push(makeValidateType())
+    validations.push(makeValidateDate())
+    validations.push(makeValidateDuration())
+    validations.push(makeValidateStatus())
+    validations.push(makeValidateTaskId())
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
