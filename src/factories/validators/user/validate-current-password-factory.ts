6897839bb9de86159c72a0ab14c5fc4b69f55155
Christian Gama <christiangama.dev@gmail.com>
2021-10-04 17:41:08 -0300

    refactor: move ValidateCurrentPassword creation to a factory method

---
+++ src/factories/validators/user/validate-current-password-factory.ts
@@ -0,0 +1,9 @@
+import { ValidateCurrentPassword } from '@/application/validators/user'
+
+import { makeBcryptAdapter } from '../../cryptography'
+
+export const makeValidateCurrentPassword = (): ValidateCurrentPassword => {
+  const hasher = makeBcryptAdapter()
+
+  return new ValidateCurrentPassword(hasher)
+}
