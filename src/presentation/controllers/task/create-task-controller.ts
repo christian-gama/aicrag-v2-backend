80813a4fdbb63b0d781b7b9a8e12b098052fc299
Christian Gama <christiangama.dev@gmail.com>
2021-10-05 11:50:55 -0300

    feat: ensure return badRequest if validation fails

--- src/presentation/controllers/task/create-task-controller.ts
+++ src/presentation/controllers/task/create-task-controller.ts
@@ -1,3 +1,5 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
 import { MustLoginError } from '@/application/errors'
 
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/http/protocols'
@@ -5,9 +7,17 @@ import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/ht
 import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class CreateTaskController implements ControllerProtocol {
-  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+  constructor (
+    private readonly createTaskValidator: ValidatorProtocol,
+    private readonly httpHelper: HttpHelperProtocol
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const data = httpRequest.body
     const user = httpRequest.user
+
+    const error = await this.createTaskValidator.validate(data)
+    if (error) return this.httpHelper.badRequest(error)
 
     if (!user) return this.httpHelper.unauthorized(new MustLoginError())
 
