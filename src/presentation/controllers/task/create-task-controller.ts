e517e81f02c554d6d7b1ddeb41d4f9e44986a2e3
Christian Gama <christiangama.dev@gmail.com>
2021-10-05 11:43:09 -0300

    feat: ensure return unauthorized if there is no user

---
+++ src/presentation/controllers/task/create-task-controller.ts
@@ -0,0 +1,16 @@
+import { MustLoginError } from '@/application/errors'
+
+import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/http/protocols'
+
+import { ControllerProtocol } from '../protocols/controller-protocol'
+
+export class CreateTaskController implements ControllerProtocol {
+  constructor (private readonly httpHelper: HttpHelperProtocol) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const user = httpRequest.user
+
+    if (!user) return this.httpHelper.unauthorized(new MustLoginError())
+
+    return this.httpHelper.ok({})
+  }
+}
