f266cb12c024d8a338d2660c9a9f1b91a92fc32a
Christian Gama <christiangama.dev@gmail.com>
2021-10-05 11:51:04 -0300

    test: ensure return badRequest if validation fails

--- tests/unit/presentation/controllers/task/create-task-controller.spec.ts
+++ tests/unit/presentation/controllers/task/create-task-controller.spec.ts
@@ -1,4 +1,5 @@
 import { IUser } from '@/domain'
+import { ValidatorProtocol } from '@/domain/validators'
 
 import { MustLoginError } from '@/application/errors'
 
@@ -7,9 +8,10 @@ import { HttpHelperProtocol, HttpRequest } from '@/presentation/http/protocols'
 
 import { makeHttpHelper } from '@/factories/helpers'
 
-import { makeFakeUser } from '@/tests/__mocks__'
+import { makeFakeUser, makeValidatorStub } from '@/tests/__mocks__'
 
 interface SutTypes {
+  createTaskValidatorStub: ValidatorProtocol
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
@@ -17,13 +19,14 @@ interface SutTypes {
 }
 
 const makeSut = (): SutTypes => {
+  const createTaskValidatorStub = makeValidatorStub()
   const fakeUser = makeFakeUser()
   const httpHelper = makeHttpHelper()
-  const request: HttpRequest = { body: '', user: fakeUser }
+  const request: HttpRequest = { body: {}, user: fakeUser }
 
-  const sut = new CreateTaskController(httpHelper)
+  const sut = new CreateTaskController(createTaskValidatorStub, httpHelper)
 
-  return { fakeUser, httpHelper, request, sut }
+  return { createTaskValidatorStub, fakeUser, httpHelper, request, sut }
 }
 
 describe('createTaskController', () => {
@@ -36,5 +39,18 @@ describe('createTaskController', () => {
     const error = await sut.handle(request)
 
     expect(error).toStrictEqual(httpHelper.unauthorized(new MustLoginError()))
+  })
+
+  it('should return badRequest if validation fails', async () => {
+    expect.hasAssertions()
+
+    const { httpHelper, request, sut, createTaskValidatorStub } = makeSut()
+    jest
+      .spyOn(createTaskValidatorStub, 'validate')
+      .mockReturnValueOnce(Promise.resolve(new Error()))
+
+    const error = await sut.handle(request)
+
+    expect(error).toStrictEqual(httpHelper.badRequest(new Error()))
   })
 })
