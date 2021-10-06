1f85c9bd5d4a16f295b5dbe1aaea66ff9f47d344
Christian Gama <christiangama.dev@gmail.com>
2021-10-06 18:12:35 -0300

    test: ensure return an error if task param is not valid

---
+++ tests/unit/application/validators/task/validate-task-param.spec.ts
@@ -0,0 +1,61 @@
+import { InvalidParamError, InvalidTypeError } from '@/application/errors'
+import { ValidateTaskParam } from '@/application/validators/task'
+
+import { HttpRequest } from '@/presentation/http/protocols'
+
+interface SutTypes {
+  request: HttpRequest
+  sut: ValidateTaskParam
+}
+const makeSut = (): SutTypes => {
+  const request: HttpRequest = { params: { id: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx' } }
+
+  const sut = new ValidateTaskParam()
+
+  return { request, sut }
+}
+
+describe('validateTaskParam', () => {
+  it('should return InvalidTypeError if id is not a string', () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+    request.params.id = 123
+
+    const error = sut.validate(request.params)
+
+    expect(error).toStrictEqual(new InvalidTypeError('id'))
+  })
+
+  it('should return InvalidParamError if there is no id', () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+    request.params.id = undefined
+
+    const error = sut.validate(request.params)
+
+    expect(error).toStrictEqual(new InvalidParamError('id'))
+  })
+
+  it('should return InvalidParamError if it is not a valid uuid', () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+    request.params.id = 'invalid_id'
+
+    const error = sut.validate(request.params)
+
+    expect(error).toStrictEqual(new InvalidParamError('id'))
+  })
+
+  it('should return undefined if succeeds', () => {
+    expect.hasAssertions()
+
+    const { request, sut } = makeSut()
+
+    const response = sut.validate(request.params)
+
+    expect(response).toBeUndefined()
+  })
+})
