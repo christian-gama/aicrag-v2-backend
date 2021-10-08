7c2a1d7e00d26fafc370351da4f9be3fb186dc91
Christian Gama <christiangama.dev@gmail.com>
2021-10-08 11:51:57 -0300

    test: ensure get /task endpoint works properly

---
+++ tests/integration/main/express/routes/task/find-all-tasks.test.ts
@@ -0,0 +1,99 @@
+import { ITask, IUser } from '@/domain'
+
+import { MongoAdapter } from '@/infra/adapters/database/mongodb'
+import { CollectionProtocol } from '@/infra/database/protocols'
+
+import app from '@/main/express/config/app'
+
+import { makeMongoDb } from '@/factories/database/mongo-db-factory'
+import { makeGenerateAccessToken, makeGenerateRefreshToken } from '@/factories/providers/token'
+
+import { makeFakeTask, makeFakeUser } from '@/tests/__mocks__'
+
+import request from 'supertest'
+
+describe('get /task', () => {
+  const client = makeMongoDb()
+  let accessToken: string
+  let fakeTask: ITask
+  let fakeUser: IUser
+  let refreshToken: string
+  let taskCollection: CollectionProtocol
+  let userCollection: CollectionProtocol
+
+  afterAll(async () => {
+    await client.disconnect()
+  })
+
+  afterEach(async () => {
+    await taskCollection.deleteMany({})
+  })
+
+  beforeAll(async () => {
+    await MongoAdapter.connect(global.__MONGO_URI__)
+
+    taskCollection = client.collection('tasks')
+    userCollection = client.collection('users')
+  })
+
+  beforeEach(async () => {
+    fakeUser = makeFakeUser()
+    fakeTask = makeFakeTask(fakeUser)
+    accessToken = makeGenerateAccessToken().generate(fakeUser)
+    refreshToken = await makeGenerateRefreshToken().generate(fakeUser)
+  })
+
+  it('should return 401 if user is not logged in', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app).get('/api/v1/task').expect(401)
+  })
+
+  it('should return 400 if query validation fails', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/task?sort=a&sort=b')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(400)
+  })
+
+  it('should return 200 with if does not find any tasks', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/task')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  })
+
+  it('should return 200 if find a task', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+    await taskCollection.insertOne(fakeTask)
+
+    await request(app)
+      .get('/api/v1/task')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  }, 12000)
+
+  it('should return 200 if all validations succeeds and find a task', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+    await taskCollection.insertOne(fakeTask)
+
+    await request(app)
+      .get('/api/v1/task?fields=duration,commentary,usd&sort=usd,-date.full&limit=2&page=1')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  }, 12000)
+})
