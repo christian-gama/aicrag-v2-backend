d6835bff4444ef1b3ab861261cd8a6fefdf4c051
Christian Gama <christiangama.dev@gmail.com>
2021-10-08 11:10:29 -0300

    test: ensure queryValidatorComposite validates all queries

---
+++ tests/integration/factories/validators/query/query-validator-composite-factory.test.ts
@@ -0,0 +1,29 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { ValidationComposite } from '@/application/validators/validation-composite'
+
+import { makeQueryValidatorComposite } from '@/factories/validators/query'
+import { makeValidateFields } from '@/factories/validators/query/validate-fields-factory'
+import { makeValidateLimit } from '@/factories/validators/query/validate-limit-factory'
+import { makeValidatePage } from '@/factories/validators/query/validate-page-factory'
+import { makeValidateSort } from '@/factories/validators/query/validate-sort-factory'
+
+jest.mock('../../../../../src/application/validators/validation-composite.ts')
+
+describe('queryValidatorComposite', () => {
+  it('should create factory with all validations', () => {
+    expect.hasAssertions()
+
+    makeQueryValidatorComposite()
+
+    const validations: ValidatorProtocol[] = []
+
+    // Must have this exact validation order
+    validations.push(makeValidateFields())
+    validations.push(makeValidateLimit())
+    validations.push(makeValidatePage())
+    validations.push(makeValidateSort())
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
