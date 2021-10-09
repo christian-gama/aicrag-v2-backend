6eac18c24ba48c52b4ca9e33a01ca74d75ec0942
Christian Gama <christiangama.dev@gmail.com>
2021-10-09 14:21:17 -0300

    test: ensure return badRequest if there is no task

--- tests/unit/presentation/controllers/task/update-task-controller.spec.ts
+++ tests/unit/presentation/controllers/task/update-task-controller.spec.ts
@@ -1,36 +1,46 @@
 import { IUser } from '@/domain'
+import { TaskDbRepositoryProtocol } from '@/domain/repositories/task/task-db-repository-protocol'
 import { ValidatorProtocol } from '@/domain/validators'
 
-import { MustLoginError } from '@/application/errors'
+import { MustLoginError, TaskNotFoundError } from '@/application/errors'
 
 import { UpdateTaskController } from '@/presentation/controllers/task'
 import { HttpHelperProtocol, HttpRequest } from '@/presentation/http/protocols'
 
 import { makeHttpHelper } from '@/factories/helpers'
 
-import { makeFakeUser, makeValidatorStub } from '@/tests/__mocks__'
+import {
+  makeFakeTask,
+  makeFakeUser,
+  makeTaskDbRepositoryStub,
+  makeValidatorStub
+} from '@/tests/__mocks__'
 
 interface SutTypes {
   fakeUser: IUser
   httpHelper: HttpHelperProtocol
   request: HttpRequest
   sut: UpdateTaskController
+  taskDbRepositoryStub: TaskDbRepositoryProtocol
   validateTaskParamStub: ValidatorProtocol
 }
 
 const makeSut = (): SutTypes => {
   const fakeUser = makeFakeUser()
+  const fakeTask = makeFakeTask(fakeUser)
   const httpHelper = makeHttpHelper()
   const request: HttpRequest = { params: { id: 'valid_id' }, user: fakeUser }
   const validateTaskParamStub = makeValidatorStub()
+  const taskDbRepositoryStub = makeTaskDbRepositoryStub(fakeTask)
 
-  const sut = new UpdateTaskController(httpHelper, validateTaskParamStub)
+  const sut = new UpdateTaskController(httpHelper, taskDbRepositoryStub, validateTaskParamStub)
 
   return {
     fakeUser,
     httpHelper,
     request,
     sut,
+    taskDbRepositoryStub,
     validateTaskParamStub
   }
 }
@@ -56,5 +66,16 @@ describe('updateTaskController', () => {
     const error = await sut.handle(request)
 
     expect(error).toStrictEqual(httpHelper.badRequest(new Error()))
+  })
+
+  it('should return badRequest if there is no task', async () => {
+    expect.hasAssertions()
+
+    const { httpHelper, request, sut, taskDbRepositoryStub } = makeSut()
+    jest.spyOn(taskDbRepositoryStub, 'findTaskById').mockReturnValueOnce(Promise.resolve(null))
+
+    const error = await sut.handle(request)
+
+    expect(error).toStrictEqual(httpHelper.badRequest(new TaskNotFoundError()))
   })
 })
