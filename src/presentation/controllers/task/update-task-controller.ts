29f6068703af0d8fb8df11a6d9b94a5c9c5cee38
Christian Gama <christiangama.dev@gmail.com>
2021-10-09 14:16:52 -0300

    feat: ensure return badRequest if param is invalid

--- src/presentation/controllers/task/update-task-controller.ts
+++ src/presentation/controllers/task/update-task-controller.ts
@@ -1,3 +1,5 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
 import { MustLoginError } from '@/application/errors'
 
 import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/http/protocols'
@@ -6,13 +8,18 @@ import { ControllerProtocol } from '../protocols/controller-protocol'
 
 export class UpdateTaskController implements ControllerProtocol {
   constructor (
-    private readonly httpHelper: HttpHelperProtocol
+    private readonly httpHelper: HttpHelperProtocol,
+    private readonly validateTaskParam: ValidatorProtocol
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const params = httpRequest.params
     const user = httpRequest.user
 
     if (!user) return this.httpHelper.unauthorized(new MustLoginError())
+
+    const error = await this.validateTaskParam.validate(params)
+    if (error) return this.httpHelper.badRequest(error)
 
     return this.httpHelper.ok({ task: '' })
   }
