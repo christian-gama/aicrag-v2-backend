e2631845ea774d5eda1fb15f32a5b158cf8a1cbd
Christian Gama <christiangama.dev@gmail.com>
2021-10-11 12:01:27 -0300

    feat: ensure GetInvoiceByMonthController return a default http response

---
+++ src/presentation/controllers/invoice/get-invoice-by-month-controller.ts
@@ -0,0 +1,34 @@
+import { InvoiceRepositoryProtocol, QueryInvoiceProtocol } from '@/domain/repositories/invoice'
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { MustLoginError } from '@/application/errors'
+
+import { HttpHelperProtocol, HttpRequest, HttpResponse } from '@/presentation/http/protocols'
+
+import { ControllerProtocol } from '../protocols/controller-protocol'
+
+export class GetInvoiceByMonthController implements ControllerProtocol {
+  constructor (
+    private readonly httpHelper: HttpHelperProtocol,
+    private readonly invoiceRepository: InvoiceRepositoryProtocol,
+    private readonly queryInvoiceValidator: ValidatorProtocol
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const user = httpRequest.user
+
+    if (!user) return this.httpHelper.unauthorized(new MustLoginError())
+
+    const error = await this.queryInvoiceValidator.validate(httpRequest.query)
+    if (error) return this.httpHelper.badRequest(error)
+
+    const query = httpRequest.query as QueryInvoiceProtocol
+
+    const result = await this.invoiceRepository.getInvoiceByMonth(query, user.personal.id)
+
+    return this.httpHelper.ok({
+      documents: result.documents,
+      page: `${result.currentPage} of ${result.totalPages}`
+    })
+  }
+}
