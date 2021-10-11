f03047e449164ed0270e6141e4e300ea0a22fbd8
Christian Gama <christiangama.dev@gmail.com>
2021-10-11 20:51:47 -0300

    test: use get-all-invoices endpoint

---
+++ tests/integration/main/express/routes/invoice/get-all-invoices.test.ts
@@ -0,0 +1,137 @@
+import { ITask, IUser } from '@/domain'
+
+import { MongoAdapter } from '@/infra/adapters/database/mongodb'
+import { CollectionProtocol } from '@/infra/database/protocols'
+
+import app from '@/main/express/config/app'
+
+import { makeMongoDb } from '@/factories/database/mongo-db-factory'
+import { makeGenerateAccessToken, makeGenerateRefreshToken } from '@/factories/providers/token'
+
+import { makeFakeTask, makeFakeUser } from '@/tests/__mocks__'
+
+import request from 'supertest'
+
+describe('get /invoice/get-all-invoices', () => {
+  const client = makeMongoDb()
+  let accessToken: string
+  let fakeTask: ITask
+  let fakeUser: IUser
+  let refreshToken: string
+  let taskCollection: CollectionProtocol
+  let userCollection: CollectionProtocol
+
+  afterAll(async () => {
+    await client.disconnect()
+  })
+
+  afterEach(async () => {
+    await taskCollection.deleteMany({})
+  })
+
+  beforeAll(async () => {
+    await MongoAdapter.connect(global.__MONGO_URI__)
+
+    taskCollection = client.collection('tasks')
+    userCollection = client.collection('users')
+  })
+
+  beforeEach(async () => {
+    fakeUser = makeFakeUser()
+    fakeTask = makeFakeTask(fakeUser)
+    accessToken = makeGenerateAccessToken().generate(fakeUser)
+    refreshToken = await makeGenerateRefreshToken().generate(fakeUser)
+  })
+
+  it('should return 401 if user is not logged in', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app).get('/api/v1/invoice/get-all-invoices').expect(401)
+  })
+
+  it('should return 400 if query validation fails', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/invoice/get-all-invoices?sort=a&sort=b')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(400)
+  })
+
+  it('should return 400 if type is invalid', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/invoice/get-all-invoices?type=invalid_type')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(400)
+  })
+
+  it('should return 400 if misses type', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/invoice/get-all-invoices')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(400)
+  })
+
+  it('should return 200 with if does not find any tasks', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+
+    await request(app)
+      .get('/api/v1/invoice/get-all-invoices?type=TX')
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  })
+
+  it('should return 200 if find a task', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+    await taskCollection.insertOne(fakeTask)
+
+    await request(app)
+      .get(`/api/v1/invoice/get-all-invoices?type=${fakeTask.type}`)
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  }, 12000)
+
+  it('should return 200 if all validations succeeds and find a task', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+    await taskCollection.insertOne(fakeTask)
+
+    await request(app)
+      .get(
+        `/api/v1/invoice/get-all-invoices?type=${fakeTask.type}&sort=usd,-date.full&limit=2&page=1`
+      )
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  }, 12000)
+
+  it('should return 200 if all validations succeeds and find a task with a taskId', async () => {
+    expect.assertions(0)
+
+    await userCollection.insertOne(fakeUser)
+    await taskCollection.insertOne(fakeTask)
+
+    await request(app)
+      .get(
+        `/api/v1/invoice/get-all-invoices?type=both&taskId=${fakeTask.taskId}&sort=usd,-date.full&limit=2&page=1`
+      )
+      .set('Cookie', `refreshToken=${refreshToken};accessToken=${accessToken}`)
+      .expect(200)
+  }, 12000)
+})
