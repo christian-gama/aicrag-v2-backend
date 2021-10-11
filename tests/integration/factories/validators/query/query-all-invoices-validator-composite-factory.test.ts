4ba056c20d9031035898c5a2868be194350363ff
Christian Gama <christiangama.dev@gmail.com>
2021-10-11 20:06:27 -0300

    test: ensure queryAllInvoicesValidatorComposite use the correct validations

---
+++ tests/integration/factories/validators/query/query-all-invoices-validator-composite-factory.test.ts
@@ -0,0 +1,39 @@
+import { ValidatorProtocol } from '@/domain/validators'
+
+import { ValidationComposite } from '@/application/validators/validation-composite'
+
+import {
+  makeValidateFields,
+  makeValidateLimit,
+  makeValidatePage,
+  makeValidateSort,
+  makeQueryAllInvoicesValidatorComposite
+} from '@/factories/validators/query'
+import { makeValidateType } from '@/factories/validators/task'
+import { makeRequiredFields } from '@/factories/validators/validate-required-fields-factory'
+
+jest.mock('../../../../../src/application/validators/validation-composite.ts')
+
+describe('queryAllInvoicesValidatorComposite', () => {
+  it('should create factory with all validations', () => {
+    expect.hasAssertions()
+
+    makeQueryAllInvoicesValidatorComposite()
+
+    const validations: ValidatorProtocol[] = []
+
+    const fields = ['type']
+    for (const field of fields) {
+      validations.push(makeRequiredFields(field))
+    }
+
+    // Must have this exact validation order
+    validations.push(makeValidateFields())
+    validations.push(makeValidateLimit())
+    validations.push(makeValidateType())
+    validations.push(makeValidatePage())
+    validations.push(makeValidateSort())
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
