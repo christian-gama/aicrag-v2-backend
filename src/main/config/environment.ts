da5a94c2ddf25918916a5037093cbe267a9f4ac5
Christian Gama <christiangama.dev@gmail.com>
2021-10-20 20:29:18 -0300

    refactor: remove useless trim

--- src/main/config/environment.ts
+++ src/main/config/environment.ts
@@ -6,27 +6,27 @@ dotenv.config({ path: path.resolve(__dirname, '../../../config.env') })
 
 export const environment = {
   COOKIES: {
-    EXPIRES: (process.env.COOKIE_EXPIRES as string).trim(),
-    SECRET: (process.env.COOKIE_SECRET as string).trim()
+    EXPIRES: (process.env.COOKIE_EXPIRES as string),
+    SECRET: (process.env.COOKIE_SECRET as string)
   },
   DB: {
-    MONGO_URL: (process.env.MONGO_URL as string).trim()
+    MONGO_URL: (process.env.MONGO_URL as string)
   },
   JWT: {
-    ACCESS_EXPIRES: (process.env.JWT_EXPIRES as string).trim().toLowerCase(),
-    ACCESS_SECRET: (process.env.JWT_SECRET as string).trim(),
-    REFRESH_EXPIRES: (process.env.JWT_REFRESH_EXPIRES as string).trim().toLowerCase(),
-    REFRESH_SECRET: (process.env.JWT_REFRESH_SECRET as string).trim()
+    ACCESS_EXPIRES: (process.env.JWT_EXPIRES as string).toLowerCase(),
+    ACCESS_SECRET: (process.env.JWT_SECRET as string),
+    REFRESH_EXPIRES: (process.env.JWT_REFRESH_EXPIRES as string).toLowerCase(),
+    REFRESH_SECRET: (process.env.JWT_REFRESH_SECRET as string)
   },
   MAILER: {
     MAILTRAP: {
-      HOST: (process.env.MAILTRAP_HOST as string).trim(),
-      PASSWORD: (process.env.MAILTRAP_PASSWORD as string).trim(),
-      PORT: (process.env.MAILTRAP_PORT as string).trim(),
-      USER: (process.env.MAILTRAP_USER as string).trim()
+      HOST: (process.env.MAILTRAP_HOST as string),
+      PASSWORD: (process.env.MAILTRAP_PASSWORD as string),
+      PORT: (process.env.MAILTRAP_PORT as string),
+      USER: (process.env.MAILTRAP_USER as string)
     },
     SENDGRID: {
-      APIKEY: (process.env.SENDGRID_APIKEY as string).trim()
+      APIKEY: (process.env.SENDGRID_APIKEY as string)
     },
     SETTINGS: {
       FROM: 'Aicrag <aicrag.team@gmail.com>'
@@ -34,10 +34,10 @@ export const environment = {
   },
   SERVER: {
     NODE_ENV:
-      ((process.env.NODE_ENV as string).trim().toLowerCase() as
+      ((process.env.NODE_ENV as string).toLowerCase() as
         | 'development'
         | 'production'
         | 'test') ?? 'production',
-    PORT: (process.env.PORT as string).trim()
+    PORT: (process.env.PORT as string)
   }
 }
