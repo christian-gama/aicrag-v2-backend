f509ef971c0911ce162f1f04c8985dfdad4e5803
Christian Gama <christiangama.dev@gmail.com>
2021-10-20 18:54:22 -0300

    refactor: remove environment credentials

--- src/main/config/environment.ts
+++ src/main/config/environment.ts
@@ -6,27 +6,27 @@ dotenv.config({ path: path.resolve(__dirname, '../../../config.env') })
 
 export const environment = {
   COOKIES: {
-    EXPIRES: process.env.COOKIE_EXPIRES?.trim() ?? '30',
-    SECRET: process.env.COOKIE_SECRET?.trim() ?? 'ebcb6513-426a-4cac-ad4a-b768b6e12d59'
+    EXPIRES: (process.env.COOKIE_EXPIRES as string).trim(),
+    SECRET: (process.env.COOKIE_SECRET as string).trim()
   },
   DB: {
-    MONGO_URL: process.env.MONGO_URL?.trim() ?? 'mongodb://mongo/aicrag'
+    MONGO_URL: (process.env.MONGO_URL as string).trim()
   },
   JWT: {
-    ACCESS_EXPIRES: process.env.JWT_EXPIRES?.trim().toLowerCase() ?? '60s',
-    ACCESS_SECRET: process.env.JWT_SECRET?.trim() ?? 'e26f414f-74cb-4f16-a1f1-db6438a1fcd6',
-    REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES?.trim().toLowerCase() ?? '30d',
-    REFRESH_SECRET: process.env.JWT_REFRESH_SECRET?.trim() ?? '12f9b733-3ea0-4edf-9e05-33c1003dc9d1'
+    ACCESS_EXPIRES: (process.env.JWT_EXPIRES as string).trim().toLowerCase(),
+    ACCESS_SECRET: (process.env.JWT_SECRET as string).trim(),
+    REFRESH_EXPIRES: (process.env.JWT_REFRESH_EXPIRES as string).trim().toLowerCase(),
+    REFRESH_SECRET: (process.env.JWT_REFRESH_SECRET as string).trim()
   },
   MAILER: {
     MAILTRAP: {
-      HOST: process.env.MAILTRAP_HOST?.trim() ?? 'smtp.mailtrap.io',
-      PASSWORD: process.env.MAILTRAP_PASSWORD?.trim(),
-      PORT: process.env.MAILTRAP_PORT?.trim() ?? '2525',
-      USER: process.env.MAILTRAP_USER?.trim()
+      HOST: (process.env.MAILTRAP_HOST as string).trim(),
+      PASSWORD: (process.env.MAILTRAP_PASSWORD as string).trim(),
+      PORT: (process.env.MAILTRAP_PORT as string).trim(),
+      USER: (process.env.MAILTRAP_USER as string).trim()
     },
     SENDGRID: {
-      APIKEY: process.env.SENDGRID_APIKEY?.trim()
+      APIKEY: (process.env.SENDGRID_APIKEY as string).trim()
     },
     SETTINGS: {
       FROM: 'Aicrag <aicrag.team@gmail.com>'
@@ -34,8 +34,10 @@ export const environment = {
   },
   SERVER: {
     NODE_ENV:
-      (process.env.NODE_ENV?.trim().toLowerCase() as 'development' | 'production' | 'test') ??
-      'production',
-    PORT: process.env.PORT?.trim() ?? '3000'
+      ((process.env.NODE_ENV as string).trim().toLowerCase() as
+        | 'development'
+        | 'production'
+        | 'test') ?? 'production',
+    PORT: (process.env.PORT as string).trim()
   }
 }
