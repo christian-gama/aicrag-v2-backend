54b2c088bca839708b62547a4a41d97ba5c3e72c
Christian Gama <christiangama.dev@gmail.com>
2021-10-24 15:29:11 -0300

    refactor: wrap message in a local const

--- src/main/express/middlewares/rate-limit.ts
+++ src/main/express/middlewares/rate-limit.ts
@@ -2,26 +2,27 @@ import { environment } from '@/main/config/environment'
 
 import rateLimit from 'express-rate-limit'
 
+const message = 'Too many requests from this IP, please try again in an hour'
 export const limiter = rateLimit({
   max: environment.SERVER.NODE_ENV === 'test' ? 0 : 120,
-  message: 'Too many requests from this IP, please try again in an hour.',
+  message,
   windowMs: 60 * 1000
 })
 
 export const signUpLimiter = rateLimit({
   max: environment.SERVER.NODE_ENV === 'test' ? 0 : 5,
-  message: 'Too many requests from this IP, please try again in an hour.',
+  message,
   windowMs: 60 * 60 * 1000
 })
 
 export const loginLimiter = rateLimit({
   max: environment.SERVER.NODE_ENV === 'test' ? 0 : 5,
-  message: 'Too many requests from this IP, please try again in an hour.',
+  message,
   windowMs: 30 * 60 * 1000
 })
 
 export const emailLimiter = rateLimit({
   max: environment.SERVER.NODE_ENV === 'test' ? 0 : 10,
-  message: 'Too many requests from this IP, please try again in an hour.',
+  message,
   windowMs: 60 * 60 * 1000
 })
