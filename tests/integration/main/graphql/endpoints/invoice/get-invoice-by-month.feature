a1506f69b9ed20725e13cfcd2677570dc130f41d
Christian Gama <christiangama.dev@gmail.com>
2021-10-27 20:09:13 -0300

    test: should get all invoces from a specific month and year using filter

--- tests/integration/main/graphql/endpoints/invoice/get-invoice-by-month.feature
+++ tests/integration/main/graphql/endpoints/invoice/get-invoice-by-month.feature
@@ -2,7 +2,7 @@ Feature: Get invoice by month
 
   I my invoices from a specific month from a year.
 
-  Scenario: I have a list of tasks from a specific month from a year
+  Scenario: I want to get all invoices from a specific month from a year
     Given I am logged in
     Given I have the following tasks:
       | commentary     | date                     | duration | status    | taskId | type |
@@ -27,3 +27,25 @@ Feature: Get invoice by month
     When I request to get my invoices from month "9" and year "2021" of type "both"
     Then I should receive an error with message "Token is missing"
     And I must receive a status code of 401
+
+  Scenario: I want to get all invoices from a specific month and year using a filter
+    Given I am logged in
+    Given I have the following tasks:
+      | commentary     | date                     | duration | status    | taskId | type |
+      | Any commentary | 2021-10-27T14:32:33.465Z | 30       | completed | 1      | TX   |
+      | Any commentary | 2021-10-24T15:12:38.773Z | 30       | completed | 2      | TX   |
+      | Any commentary | 2021-10-15T08:41:54.351Z | 30       | completed | 3      | TX   |
+      | Any commentary | 2021-10-19T07:11:15.912Z | 30       | completed | 4      | TX   |
+      | Any commentary | 2021-08-31T23:59:59.999Z | 2.4      | completed | 5      | QA   |
+      | Any commentary | 2022-01-01T00:00:00.000Z | 15       | completed | 6      | TX   |
+    # Month starts counting from 0 to 11.
+    When I request to get my invoices from month "9" and year "2021" of type "both" limiting by "1"
+    Then I should get the following invoice:
+      | commentary     | date                     | duration | status    | taskId | type | usd  |
+      | Any commentary | 2021-10-27T14:32:33.465Z | 30       | completed | 1      | TX   | 32.5 |
+      | Any commentary | 2021-10-24T15:12:38.773Z | 30       | completed | 2      | TX   | 32.5 |
+      | Any commentary | 2021-10-15T08:41:54.351Z | 30       | completed | 3      | TX   | 32.5 |
+      | Any commentary | 2021-10-19T07:11:15.912Z | 30       | completed | 4      | TX   | 32.5 |
+    And The displaying count should be "1"
+    And The amount page should be "1 of 4"
+    And I must receive a status code of 200
--- tests/integration/main/graphql/endpoints/invoice/get-invoice-by-month.steps.ts
+++ tests/integration/main/graphql/endpoints/invoice/get-invoice-by-month.steps.ts
@@ -51,7 +51,7 @@ defineFeature(feature, (test) => {
     taskCollection = client.collection('tasks')
   })
 
-  test('i have a list of tasks from a specific month from a year', ({ given, when, then, and }) => {
+  test('i want to get all invoices from a specific month from a year', ({ given, when, then, and }) => {
     expect.hasAssertions()
 
     given('I am logged in', async () => {
@@ -168,6 +168,103 @@ defineFeature(feature, (test) => {
 
     then(/^I should receive an error with message "(.*)"$/, (message) => {
       expect(result.body.errors[0].message).toBe(message)
+    })
+
+    and(/^I must receive a status code of (.*)$/, (statusCode) => {
+      expect(result.status).toBe(parseInt(statusCode))
+    })
+  })
+
+  test('i want to get all invoices from a specific month and year using a filter', ({ given, when, then, and }) => {
+    expect.hasAssertions()
+
+    given('I am logged in', async () => {
+      fakeUser = await userHelper.insertUser(userCollection)
+      ;[accessToken, refreshToken] = await userHelper.generateToken(fakeUser)
+    })
+
+    given('I have the following tasks:', async (table) => {
+      tasks = await Promise.all(
+        table.map(async (row) => {
+          const date = new Date(Date.parse(row.date))
+
+          return await taskHelper.insertTask(taskCollection, fakeUser, {
+            commentary: row.commentary,
+            date: {
+              day: date.getUTCDate(),
+              full: date,
+              hours: date.toLocaleTimeString('pt-br', { timeZone: 'UTC' }),
+              month: date.getUTCMonth(),
+              year: date.getUTCFullYear()
+            },
+            duration: +row.duration,
+            status: row.status,
+            taskId: row.taskId,
+            type: row.type,
+            usd: Math.round((+row.duration / 60) * (row.type === 'TX' ? 65 : 112.5) * 100) / 100
+          })
+        })
+      )
+    })
+
+    when(
+      /^I request to get my invoices from month "(.*)" and year "(.*)" of type "(.*)" limiting by "(.*)"$/,
+      async (month, year, type, limit) => {
+        const query = getInvoiceByMonthQuery({ limit, month, type, year })
+
+        result = await request(app)
+          .post('/graphql')
+          .set('x-access-token', accessToken)
+          .set('x-refresh-token', refreshToken)
+          .send({ query })
+      }
+    )
+
+    then('I should get the following invoice:', (table) => {
+      const invoice = table.map((row, index) => {
+        const date = new Date(Date.parse(row.date))
+
+        return {
+          commentary: row.commentary,
+          date: {
+            day: date.getUTCDate(),
+            full: date.toISOString(),
+            hours: date.toLocaleTimeString('pt-br', { timeZone: 'UTC' }),
+            month: date.getUTCMonth(),
+            year: date.getUTCFullYear()
+          },
+          duration: +row.duration,
+          id: tasks[index].id,
+          logs: {
+            createdAt: tasks[index].logs.createdAt.toISOString(),
+            updatedAt: tasks[index].logs.updatedAt
+          },
+          status: row.status,
+          taskId: row.taskId,
+          type: row.type,
+          usd: Math.round((+row.duration / 60) * (row.type === 'TX' ? 65 : 112.5) * 100) / 100,
+          user: {
+            personal: {
+              email: fakeUser.personal.email,
+              id: fakeUser.personal.id,
+              name: fakeUser.personal.name
+            },
+            settings: {
+              currency: fakeUser.settings.currency
+            }
+          }
+        }
+      })
+
+      expect(result.body.data.getInvoiceByMonth.documents).toStrictEqual([invoice[0]])
+    })
+
+    and(/^The displaying count should be "(.*)"$/, (displaying) => {
+      expect(result.body.data.getInvoiceByMonth.displaying).toBe(parseInt(displaying))
+    })
+
+    and(/^The amount page should be "(.*)"$/, (page) => {
+      expect(result.body.data.getInvoiceByMonth.page).toBe(page)
     })
 
     and(/^I must receive a status code of (.*)$/, (statusCode) => {
