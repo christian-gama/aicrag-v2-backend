05a930d34c5416b2e7620b7a1ab55ab0e6aa6080
Christian Gama <christiangama.dev@gmail.com>
2021-10-28 00:39:27 -0300

    test: should update password if user have a valid reset password token

--- tests/integration/main/graphql/endpoints/login/reset-password.feature
+++ tests/integration/main/graphql/endpoints/login/reset-password.feature
@@ -7,3 +7,10 @@ Feature: Reset password
     When I request to reset my password with my new password "12345678"
     Then I should receive an error message "You must logout first"
     And I must receive a status code of 403
+
+  Scenario: Using valid input
+    Given I have a valid reset password token
+    Given I am logged out
+    When I request to reset my password with my new password "12345678"
+    Then I should have my password changed
+    And I must receive a status code of 200
--- tests/integration/main/graphql/endpoints/login/reset-password.steps.ts
+++ tests/integration/main/graphql/endpoints/login/reset-password.steps.ts
@@ -6,7 +6,9 @@ import { ICollectionMethods } from '@/infra/database/protocols'
 import { setupApp } from '@/main/express/config/app'
 
 import { makeMongoDb } from '@/factories/database/mongo-db-factory'
+import { makeGenerateAccessToken } from '@/factories/providers/token'
 
+import { makeFakeUser } from '@/tests/__mocks__'
 import { resetPasswordMutation } from '@/tests/helpers/queries'
 import { userHelper } from '@/tests/helpers/user-helper'
 
@@ -62,6 +64,42 @@ defineFeature(feature, (test) => {
 
     then(/^I should receive an error message "(.*)"$/, (message) => {
       expect(result.body.errors[0].message).toBe(message)
+    })
+
+    and(/^I must receive a status code of (.*)$/, (statusCode) => {
+      expect(result.status).toBe(parseInt(statusCode))
+    })
+  })
+
+  test('using valid input', ({ given, when, then, and }) => {
+    expect.hasAssertions()
+
+    given('I have a valid reset password token', async () => {
+      fakeUser = makeFakeUser()
+      const token = makeGenerateAccessToken().generate(fakeUser)
+      fakeUser.temporary.resetPasswordToken = token
+      await userCollection.insertOne(fakeUser)
+      accessToken = token
+    })
+
+    given('I am logged out', async () => {
+      refreshToken = ''
+    })
+
+    when(/^I request to reset my password with my new password "(.*)"$/, async (password) => {
+      const query = resetPasswordMutation({ password, passwordConfirmation: password })
+
+      result = await request(app)
+        .post('/graphql')
+        .set('x-access-token', accessToken)
+        .set('x-refresh-token', refreshToken)
+        .send({ query })
+    })
+
+    then('I should have my password changed', async () => {
+      const user = (await userCollection.findOne({ 'personal.id': fakeUser.personal.id })) as IUser
+
+      expect(user.personal.password).not.toBe(fakeUser.personal.password)
     })
 
     and(/^I must receive a status code of (.*)$/, (statusCode) => {
