7922610193de6fb4a73ae2c401ec753d1f16f543
Christian Gama <christiangama.dev@gmail.com>
2021-10-27 23:11:51 -0300

    test: should not be able to activate an account using an invalid activation code

--- tests/integration/main/graphql/endpoints/login/activate-account.feature
+++ tests/integration/main/graphql/endpoints/login/activate-account.feature
@@ -12,3 +12,14 @@ Feature: Activate account
       | any_email@mail.com | 12345          |
     Then I should have my account activated
     And I must receive a status code of 200
+
+  Scenario: I should not be able to activate my account using an invalid activation code
+    Given I have an account with the following credentials:
+      | email              | activationCode |
+      | any_email@mail.com | 12345          |
+    Given I am partially logged in
+    When I request to activate my account using the following credentials:
+      | email              | activationCode |
+      | any_email@mail.com | 54321          |
+    Then I should receive an error message "Invalid code"
+    And I must receive a status code of 400
--- tests/integration/main/graphql/endpoints/login/activate-account.steps.ts
+++ tests/integration/main/graphql/endpoints/login/activate-account.steps.ts
@@ -86,4 +86,45 @@ defineFeature(feature, (test) => {
       expect(result.status).toBe(parseInt(statusCode))
     })
   })
+
+  test('i should not be able to activate my account using an invalid activation code', ({ given, when, then, and }) => {
+    expect.hasAssertions()
+
+    given('I have an account with the following credentials:', async (table) => {
+      fakeUser = await userHelper.insertUser(userCollection, {
+        personal: {
+          email: table[0].email,
+          id: randomUUID(),
+          name: 'any_name',
+          password: 'any_password'
+        },
+        temporary: {
+          activationCode: table[0].activationCode,
+          activationCodeExpiration: new Date(Date.now() + 1000 * 60 * 10)
+        }
+      })
+    })
+
+    given('I am partially logged in', async () => {
+      ;[accessToken, refreshToken = ''] = await userHelper.generateToken(fakeUser)
+    })
+
+    when('I request to activate my account using the following credentials:', async (table) => {
+      const query = activateAccountMutation({ activationCode: table[0].activationCode, email: table[0].email })
+
+      result = await request(app)
+        .post('/graphql')
+        .set('x-access-token', accessToken)
+        .set('x-refresh-token', refreshToken)
+        .send({ query })
+    })
+
+    then(/^I should receive an error message "(.*)"$/, (message) => {
+      expect(result.body.errors[0].message).toBe(message)
+    })
+
+    and(/^I must receive a status code of (.*)$/, (statusCode) => {
+      expect(result.status).toBe(parseInt(statusCode))
+    })
+  })
 })
