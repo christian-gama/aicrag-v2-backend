d730d756a9ac166073ba8b7cf02f240f80cbe236
Christian Gama <christiangama.dev@gmail.com>
2021-10-31 18:14:19 -0300

    feat: should return an error if period is invalid

---
+++ src/application/validators/query/validate-period.ts
@@ -0,0 +1,20 @@
+import { IValidator } from '@/domain/validators'
+
+import { InvalidQueryError } from '@/application/errors'
+
+export class ValidatePeriod implements IValidator {
+  async validate (input: any): Promise<InvalidQueryError | undefined> {
+    if (!input.period) return
+
+    if (typeof input.period !== 'string') return new InvalidQueryError('period')
+
+    if (
+      input.period !== 'today' &&
+      input.period !== 'past_3_days' &&
+      input.period !== 'past_7_days' &&
+      input.period !== 'past_15_days'
+    ) {
+      return new InvalidQueryError('period')
+    }
+  }
+}
