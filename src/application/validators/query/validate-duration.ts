4432d963e8e5cf88c8cb8401596845878981504a
Christian Gama <christiangama.dev@gmail.com>
2021-11-01 01:30:42 -0300

    feat: ensure duration and operator that come from query is valid

---
+++ src/application/validators/query/validate-duration.ts
@@ -0,0 +1,15 @@
+import { IValidator } from '@/domain/validators'
+
+import { InvalidQueryError } from '@/application/errors'
+
+export class ValidateDuration implements IValidator {
+  async validate (input: any): Promise<InvalidQueryError | undefined> {
+    if (input.operator && input.operator !== 'gte' && input.operator !== 'lte' && input.operator !== 'eq') {
+      return new InvalidQueryError('operator')
+    }
+
+    if (input.duration && (input.duration <= 0 || input.duration > 30)) {
+      return new InvalidQueryError('duration')
+    }
+  }
+}
