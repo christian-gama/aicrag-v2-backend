3c544152697a336df684be3db33016e25d304208
Christian Gama <christiangama.dev@gmail.com>
2021-11-08 11:52:45 -0300

    test: add custom permission error

---
+++ src/application/errors/__tests__/permission-error.spec.ts
@@ -0,0 +1,17 @@
+import { PermissionError } from '@/application/errors'
+
+describe('invalidQueryError', () => {
+  it('should be an instance of Error', () => {
+    const sut = new PermissionError()
+
+    expect(sut).toBeInstanceOf(Error)
+  })
+
+  it('should return a message with the field name', () => {
+    const sut = new PermissionError()
+
+    const result = sut.message
+
+    expect(result).toBe('You have no permission')
+  })
+})
