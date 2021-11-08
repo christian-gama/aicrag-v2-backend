d7ca69562739ed0dc3b86dc1baefcb7eafc1fe02
Christian Gama <christiangama.dev@gmail.com>
2021-11-08 12:22:34 -0300

    test: ensure the user is an administrator

---
+++ src/presentation/middlewares/__tests__/administrator-middleware.spec.ts
@@ -0,0 +1,53 @@
+import { MustLoginError, PermissionError } from '@/application/errors'
+import { IUser } from '@/domain'
+import { makeHttpHelper } from '@/main/factories/helpers'
+import { HttpRequest, IHttpHelper } from '@/presentation/http/protocols'
+import { makeFakeUser } from '@/tests/__mocks__'
+import { AdministratorMiddleware } from '..'
+
+interface SutTypes {
+  fakeUser: IUser
+  httpHelper: IHttpHelper
+  request: HttpRequest
+  sut: AdministratorMiddleware
+}
+
+const makeSut = (): SutTypes => {
+  const fakeUser = makeFakeUser({ settings: { role: 'administrator' } })
+  const httpHelper = makeHttpHelper()
+  const request = { user: fakeUser }
+
+  const sut = new AdministratorMiddleware(httpHelper)
+
+  return { fakeUser, httpHelper, request, sut }
+}
+
+describe('administratorMiddleware', () => {
+  it('should return ok if user is an administrator', async () => {
+    const { sut, httpHelper, request } = makeSut()
+
+    const httpResponse = await sut.handle(request)
+
+    expect(httpResponse).toStrictEqual(httpHelper.ok({}))
+  })
+
+  it('should return unauthorized with MustLoginError if the user is not logged in', async () => {
+    const { sut, httpHelper, request } = makeSut()
+
+    delete request.user
+
+    const httpResponse = await sut.handle(request)
+
+    expect(httpResponse).toStrictEqual(httpHelper.unauthorized(new MustLoginError()))
+  })
+
+  it('should return forbidden with PermissionError if the user is not an administrator', async () => {
+    const { sut, httpHelper, request } = makeSut()
+
+    ;(request.user as IUser).settings.role = 'user'
+
+    const httpResponse = await sut.handle(request)
+
+    expect(httpResponse).toStrictEqual(httpHelper.forbidden(new PermissionError()))
+  })
+})
