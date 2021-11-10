5161c866077f649a9755dcb9e21586dd1bf69cd8
Christian Gama <christiangama.dev@gmail.com>
2021-11-10 14:33:08 -0300

    test: ensure role is valid

---
+++ src/application/validators/query/__tests__/validate-role.spec.ts
@@ -0,0 +1,53 @@
+import { InvalidQueryError } from '@/application/errors'
+import { ValidateRole } from '@/application/validators/query'
+import { HttpRequest } from '@/presentation/http/protocols'
+
+interface SutTypes {
+  request: HttpRequest
+  sut: ValidateRole
+}
+const makeSut = (): SutTypes => {
+  const request: HttpRequest = { query: { role: 'administrator' } }
+
+  const sut = new ValidateRole()
+
+  return { request, sut }
+}
+
+describe('validateRole', () => {
+  it('should return InvalidQueryError if role is not a string', async () => {
+    const { request, sut } = makeSut()
+    request.query.role = 123
+
+    const result = await sut.validate(request.query)
+
+    expect(result).toStrictEqual(new InvalidQueryError('role'))
+  })
+
+  it('should return InvalidQueryError if role is invalid', async () => {
+    const { request, sut } = makeSut()
+    request.query.role = 'invalid_role'
+
+    const result = await sut.validate(request.query)
+
+    expect(result).toStrictEqual(new InvalidQueryError('role'))
+  })
+
+  it('should return undefined if succeeds', async () => {
+    const { request, sut } = makeSut()
+
+    const result = await sut.validate(request.query)
+
+    expect(result).toBeUndefined()
+  })
+
+  it('should return undefined if there is no role', async () => {
+    const { request, sut } = makeSut()
+
+    delete request.query.role
+
+    const result = await sut.validate(request.query)
+
+    expect(result).toBeUndefined()
+  })
+})
