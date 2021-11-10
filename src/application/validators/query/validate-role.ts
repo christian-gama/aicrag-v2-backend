65397ddb2f0c6e7017ed7c852c82be1982b50717
Christian Gama <christiangama.dev@gmail.com>
2021-11-10 14:29:34 -0300

    feat: ensure role is valid

---
+++ src/application/validators/query/validate-role.ts
@@ -0,0 +1,19 @@
+import { IValidator } from '@/domain/validators'
+import { InvalidQueryError } from '@/application/errors'
+
+export class ValidateRole implements IValidator {
+  async validate (input: any): Promise<InvalidQueryError | undefined> {
+    if (!input.role) return
+
+    if (typeof input.role !== 'string') return new InvalidQueryError('role')
+
+    if (
+      input.role !== 'administrator' &&
+      input.role !== 'moderator' &&
+      input.role !== 'user' &&
+      input.role !== 'guest'
+    ) {
+      return new InvalidQueryError('role')
+    }
+  }
+}
