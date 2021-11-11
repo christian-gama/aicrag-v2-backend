157c66bdd2bceced5228574928ea85bd6fc6bdea
Christian Gama <christiangama.dev@gmail.com>
2021-11-11 19:01:03 -0300

    refactor: remove useless console.log

--- src/main/graphql/config/context.ts
+++ src/main/graphql/config/context.ts
@@ -2,7 +2,6 @@ import { makeUserDataLoader } from './data-loaders'
 
 export const context = async ({ req, res }): Promise<Record<string, any>> => {
   const userDataLoader = makeUserDataLoader()
-  console.log('CONTEXT')
 
   return { req, res, userDataLoader }
 }
