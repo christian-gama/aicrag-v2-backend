de83d82751771d27f12143cae2402cecd177d494
Christian Gama <christiangama.dev@gmail.com>
2021-11-12 21:33:38 -0300

    feat: ensure return an error if email is invalid

--- src/presentation/controllers/dashboard/users/update-user-controller.ts
+++ src/presentation/controllers/dashboard/users/update-user-controller.ts
@@ -3,13 +3,24 @@ import { HttpRequest, HttpResponse, IHttpHelper } from '@/presentation/http/prot
 import { IController } from '../../protocols/controller.model'
 
 export class UpdateUserController implements IController {
-  constructor (private readonly httpHelper: IHttpHelper, private readonly validateName: IValidator) {}
+  constructor (
+    private readonly httpHelper: IHttpHelper,
+    private readonly validateEmail: IValidator,
+    private readonly validateName: IValidator
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const data = httpRequest.body
 
     if (data.name) {
       const error = await this.validateName.validate(data.name)
+      if (error) {
+        return this.httpHelper.badRequest(error)
+      }
+    }
+
+    if (data.email) {
+      const error = await this.validateEmail.validate(data.email)
       if (error) {
         return this.httpHelper.badRequest(error)
       }
