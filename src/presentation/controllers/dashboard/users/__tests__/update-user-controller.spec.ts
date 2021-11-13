0d7fa9896b42796b036051db638fcb571514780f
Christian Gama <christiangama.dev@gmail.com>
2021-11-12 21:33:53 -0300

    test: ensure return an error if email is invalid

--- src/presentation/controllers/dashboard/users/__tests__/update-user-controller.spec.ts
+++ src/presentation/controllers/dashboard/users/__tests__/update-user-controller.spec.ts
@@ -7,15 +7,17 @@ import { UpdateUserController } from '../'
 interface SutTypes {
   httpHelper: IHttpHelper
   sut: UpdateUserController
+  validateEmailStub: IValidator
   validateNameStub: IValidator
 }
 const makeSut = (): SutTypes => {
   const httpHelper = makeHttpHelper()
+  const validateEmailStub = makeValidatorStub()
   const validateNameStub = makeValidatorStub()
 
-  const sut = new UpdateUserController(httpHelper, validateNameStub)
+  const sut = new UpdateUserController(httpHelper, validateEmailStub, validateNameStub)
 
-  return { httpHelper, sut, validateNameStub }
+  return { httpHelper, sut, validateEmailStub, validateNameStub }
 }
 
 describe('updateUserController', () => {
@@ -24,6 +26,15 @@ describe('updateUserController', () => {
     jest.spyOn(validateNameStub, 'validate').mockReturnValueOnce(new Error())
 
     const result = await sut.handle({ body: { name: 'invalid_name' } })
+
+    expect(result).toStrictEqual(httpHelper.badRequest(new Error()))
+  })
+
+  it('should return an error if tries to update email and email is invalid', async () => {
+    const { httpHelper, sut, validateEmailStub } = makeSut()
+    jest.spyOn(validateEmailStub, 'validate').mockReturnValueOnce(new Error())
+
+    const result = await sut.handle({ body: { email: 'invalid_email' } })
 
     expect(result).toStrictEqual(httpHelper.badRequest(new Error()))
   })
