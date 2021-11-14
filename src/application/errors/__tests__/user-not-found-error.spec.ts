d8c59c0271324a60e9dd9733bf57683e353ad0bd
Christian Gama <christiangama.dev@gmail.com>
2021-11-14 11:31:27 -0300

    test: ensure error is instance of Error and return a default message

---
+++ src/application/errors/__tests__/user-not-found-error.spec.ts
@@ -0,0 +1,17 @@
+import { UserNotFoundError } from '@/application/errors'
+
+describe('taskNotFoundError', () => {
+  it('should be an instance of Error', () => {
+    const sut = new UserNotFoundError()
+
+    expect(sut).toBeInstanceOf(Error)
+  })
+
+  it('should return a message with the field name', () => {
+    const sut = new UserNotFoundError()
+
+    const result = sut.message
+
+    expect(result).toBe('No users were found')
+  })
+})
