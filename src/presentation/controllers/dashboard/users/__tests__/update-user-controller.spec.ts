6a2ed452bfd0271dda4f5fe20aa0bfdf0398d921
Christian Gama <christiangama.dev@gmail.com>
2021-11-14 11:34:20 -0300

    test: ensure the user is updated

--- src/presentation/controllers/dashboard/users/__tests__/update-user-controller.spec.ts
+++ src/presentation/controllers/dashboard/users/__tests__/update-user-controller.spec.ts
@@ -1,34 +1,227 @@
+import { IUser } from '@/domain'
+import { IUserRepository } from '@/domain/repositories'
 import { IValidator } from '@/domain/validators'
-import { IHttpHelper } from '@/presentation/http/protocols'
+import { MustLoginError } from '@/application/errors'
+import { IHttpHelper, HttpRequest } from '@/presentation/http/protocols'
 import { makeHttpHelper } from '@/main/factories/helpers'
-import { makeValidatorStub } from '@/tests/__mocks__'
-import { UpdateUserController } from '../'
+import { makeFakeUser, makeUserRepositoryStub, makeValidatorStub } from '@/tests/__mocks__'
+import MockDate from 'mockdate'
+import { UpdateUserController } from '..'
 
 interface SutTypes {
+  fakeUser: IUser
   httpHelper: IHttpHelper
+  request: HttpRequest
   sut: UpdateUserController
   updateUserValidatorStub: IValidator
+  userRepositoryStub: IUserRepository
 }
+
 const makeSut = (): SutTypes => {
+  const fakeUser = makeFakeUser()
   const httpHelper = makeHttpHelper()
+  const request: HttpRequest = {
+    body: {
+      accountStatus: 'active',
+      email: fakeUser.personal.email,
+      handicap: 0.8,
+      name: fakeUser.personal.name,
+      role: 'moderator',
+      tokenVersion: 2
+    },
+    user: fakeUser
+  }
+  const userRepositoryStub = makeUserRepositoryStub(fakeUser)
   const updateUserValidatorStub = makeValidatorStub()
 
-  const sut = new UpdateUserController(httpHelper, updateUserValidatorStub)
+  const sut = new UpdateUserController(httpHelper, updateUserValidatorStub, userRepositoryStub)
 
   return {
+    fakeUser,
     httpHelper,
+    request,
     sut,
-    updateUserValidatorStub
+    updateUserValidatorStub,
+    userRepositoryStub
   }
 }
 
 describe('updateUserController', () => {
-  it('should return an error if validation fails', async () => {
-    const { httpHelper, sut, updateUserValidatorStub } = makeSut()
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  it('should return unauthorized if user is not logged in', async () => {
+    const { httpHelper, request, sut } = makeSut()
+    delete request.user
+
+    const result = await sut.handle(request)
+
+    expect(result).toStrictEqual(httpHelper.unauthorized(new MustLoginError()))
+  })
+
+  it('should call updateUserValidator with correct data', async () => {
+    const { request, sut, updateUserValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(updateUserValidatorStub, 'validate')
+
+    await sut.handle(request)
+
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+
+  it('should return badRequest if updateUserValidator fails', async () => {
+    const { httpHelper, request, sut, updateUserValidatorStub } = makeSut()
     jest.spyOn(updateUserValidatorStub, 'validate').mockReturnValueOnce(new Error())
 
-    const result = await sut.handle({ body: { any_field: 'invalid_field' } })
+    const result = await sut.handle(request)
 
     expect(result).toStrictEqual(httpHelper.badRequest(new Error()))
+  })
+
+  it('should call updateById with correct values if only accountStatus is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.email
+    delete request.body.handicap
+    delete request.body.name
+    delete request.body.role
+    delete request.body.tokenVersion
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'settings.activateAccount': request.body.accountStatus === 'active'
+    })
+  })
+
+  it('should call updateById with correct values if only email is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.accountStatus
+    delete request.body.handicap
+    delete request.body.name
+    delete request.body.role
+    delete request.body.tokenVersion
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'personal.email': request.body.email
+    })
+  })
+
+  it('should call updateById with correct values if only handicap is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.accountStatus
+    delete request.body.email
+    delete request.body.name
+    delete request.body.role
+    delete request.body.tokenVersion
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'settings.handicap': request.body.handicap
+    })
+  })
+
+  it('should call updateById with correct values if only name is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.accountStatus
+    delete request.body.email
+    delete request.body.handicap
+    delete request.body.role
+    delete request.body.tokenVersion
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'personal.name': request.body.name
+    })
+  })
+
+  it('should call updateById with correct values if only role is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.accountStatus
+    delete request.body.email
+    delete request.body.handicap
+    delete request.body.name
+    delete request.body.tokenVersion
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'settings.role': request.body.role
+    })
+  })
+
+  it('should call updateById with correct values if only tokenVersion is changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+    delete request.body.accountStatus
+    delete request.body.email
+    delete request.body.handicap
+    delete request.body.name
+    delete request.body.role
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      tokenVersion: request.body.tokenVersion
+    })
+  })
+
+  it('should call updateById with correct values if accountStatus, email, handicap, name, role and tokenVersion are changed', async () => {
+    const { fakeUser, request, sut, userRepositoryStub } = makeSut()
+    const updateMeSpy = jest.spyOn(userRepositoryStub, 'updateById')
+
+    await sut.handle(request)
+
+    expect(updateMeSpy).toHaveBeenCalledWith(fakeUser.personal.id, {
+      'logs.updatedAt': new Date(Date.now()),
+      'personal.email': request.body.email,
+      'personal.name': request.body.name,
+      'settings.activateAccount': request.body.accountStatus === 'active',
+      'settings.handicap': request.body.handicap,
+      'settings.role': request.body.role,
+      tokenVersion: request.body.tokenVersion
+    })
+  })
+
+  it('should return ok if succeeds with no changes', async () => {
+    const { httpHelper, request, sut } = makeSut()
+    delete request.body.accountStatus
+    delete request.body.email
+    delete request.body.handicap
+    delete request.body.name
+    delete request.body.role
+    delete request.body.tokenVersion
+
+    const result = await sut.handle(request)
+
+    expect(result).toStrictEqual(httpHelper.ok({ message: 'No changes were made' }))
+  })
+
+  it('should return ok if succeeds', async () => {
+    const { fakeUser, httpHelper, request, sut } = makeSut()
+
+    const result = await sut.handle(request)
+
+    delete (fakeUser.personal as any).password
+
+    expect(result).toStrictEqual(httpHelper.ok({ user: fakeUser }))
   })
 })
