ca7e490a5f1a054856c37be8c1e6d2da408079d3
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-13 10:33:17 -0300

    feat: add production origin

--- src/main/express/middlewares/cors.ts
+++ src/main/express/middlewares/cors.ts
@@ -1,8 +1,9 @@
+import { environment } from '@/main/config/environment'
 import _cors from 'cors'
 
 export const cors = _cors({
   credentials: true,
   exposedHeaders: ['x-access-token', 'x-refresh-token'],
   methods: 'GET,PATCH,POST,DELETE',
-  origin: 'http://localhost:3000'
+  origin: environment.SERVER.NODE_ENV === 'production' ? 'https://aicrag.netlify.app' : 'http://localhost:3000'
 })
